03ff8bf5fc287584f97a7767e758365469b740ba
Tarrence van As <tarrence@vanas.family>
2022-05-02 21:16:25 -0400

    fix(jsonrpc): test method name

--- jsonrpc/client_test.go
+++ jsonrpc/client_test.go
@@ -41,7 +41,7 @@ func TestJsonRpcClient(t *testing.T) {
 		t.Errorf("Could not retrieve block: %v\n", err)
 	}
 
-	_, err = client.CodeByAddress(context.Background(), "0x050c47150563ff7cf60dd60f7d0bd4d62a9cc5331441916e5099e905bdd8c4bc")
+	_, err = client.CodeAt(context.Background(), "0x050c47150563ff7cf60dd60f7d0bd4d62a9cc5331441916e5099e905bdd8c4bc")
 	if err != nil {
 		t.Errorf("Could not retrieve code: %v\n", err)
 	}
