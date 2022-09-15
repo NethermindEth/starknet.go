03a58f9e36ec24ba5ff4bca238ae06650f0faacd
Gregory Edison <gregory.edison1993@gmail.com>
2022-09-15 12:13:06 +0200

    feat: uint256 type checks

--- types/uint256.go
+++ types/uint256.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"fmt"
 	"math/big"
 )
 
@@ -9,11 +10,17 @@ type Uint256 struct {
 	High *Felt
 }
 
-func NewUint256(low, high *Felt) *Uint256 {
+func NewUint256(low, high *Felt) (*Uint256, error) {
+	if low.Cmp(new(big.Int).Lsh(big.NewInt(1), 128)) >= 0 {
+		return nil, fmt.Errorf("invalid low felt value")
+	}
+	if high.Cmp(new(big.Int).Lsh(big.NewInt(1), 128)) >= 0 {
+		return nil, fmt.Errorf("invalid high felt value")
+	}
 	return &Uint256{
 		Low:  low,
 		High: high,
-	}
+	}, nil
 }
 
 func (u *Uint256) Big() *big.Int {
@@ -24,9 +31,12 @@ func (u *Uint256) String() string {
 	return u.Big().String()
 }
 
-func Uint256FromBig(b *big.Int) *Uint256 {
+func Uint256FromBig(b *big.Int) (*Uint256, error) {
+	if b.Cmp(new(big.Int).Lsh(big.NewInt(1), 256)) >= 0 {
+		return nil, fmt.Errorf("invalid uint256 value")
+	}
 	return &Uint256{
 		Low:  &Felt{Int: new(big.Int).Mod(b, new(big.Int).Exp(big.NewInt(2), big.NewInt(128), nil))},
 		High: &Felt{Int: new(big.Int).Rsh(b, 128)},
-	}
+	}, nil
 }
