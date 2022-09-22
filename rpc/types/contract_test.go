61ccc5ad933a54efb7809123dd36464f07b1955d
Zoraiz Mahmood <35128199+rzmahmood@users.noreply.github.com>
2022-09-22 16:56:32 +1000

    Minor linting fix

--- rpc/types/contract_test.go
+++ rpc/types/contract_test.go
@@ -6,8 +6,10 @@ import (
 	"testing"
 )
 
-const validContractCompiledPath = "./testdata/cairo/minimum_contract_compiled.json"
-const invalidContractCompiledPath = "./testdata/cairo/invalid_minimum_contract_compiled.json"
+const (
+	validContractCompiledPath   = "./testdata/cairo/minimum_contract_compiled.json"
+	invalidContractCompiledPath = "./testdata/cairo/invalid_minimum_contract_compiled.json"
+)
 
 func TestContractClass_UnmarshalValidJSON_Successful(t *testing.T) {
 	content, err := os.ReadFile(validContractCompiledPath)
