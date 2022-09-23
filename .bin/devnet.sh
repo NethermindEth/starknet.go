48e7a69dc0cc24311dd0116e81bd8cb42ab22949
gregory <10611760+gregoryguillou@users.noreply.github.com>
2022-09-23 17:41:49 +0200

    mint ETH to the account

--- .bin/devnet.sh
+++ .bin/devnet.sh
@@ -8,6 +8,9 @@ while true; do
   curl --fail localhost:5050/is_alive 2>/dev/null 2>&1
   result=$?
   if [ $result -eq 0 ]; then
+    sleep 5
+    curl --fail -H 'Content-Type: application/json' -XPOST http://localhost:5050/mint \
+      -d '{ "address": "0x06bb9425718d801fd06f144abb82eced725f0e81db61d2f9f4c9a26ece46a829", "amount": 1000000000000000}'
     exit 0
   fi
   if [ $i -gt 10  ]; then
