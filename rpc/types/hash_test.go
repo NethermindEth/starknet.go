e7faa71898796910bf5b1a542b65a992f63f8376
gregory <10611760+gregoryguillou@users.noreply.github.com>
2022-10-04 02:03:12 +0200

    fix hash tests

--- rpc/types/hash_test.go
+++ rpc/types/hash_test.go
@@ -11,17 +11,17 @@ func TestHash(t *testing.T) {
 	for _, tc := range []struct {
 		hash string
 	}{{
-		hash: "0x315e364b162653e5c7b23efd34f8da27ba9c069b68e3042b7d76ce1df890313",
+		hash: "0x0315e364b162653e5c7b23efd34f8da27ba9c069b68e3042b7d76ce1df890313",
 	}, {
-		hash: "0x8d6955e1bc0d5ba78b04630375f962ce6e5c91115173bc5f6e7843c6ee1269",
+		hash: "0x008d6955e1bc0d5ba78b04630375f962ce6e5c91115173bc5f6e7843c6ee1269",
 	}, {
-		hash: "0x680b0616e65633dfaf06d5a5ee5f1d1d4b641396009f00a67c0d18dc0f9638",
+		hash: "0x00680b0616e65633dfaf06d5a5ee5f1d1d4b641396009f00a67c0d18dc0f9638",
 	}} {
-		var th TransactionHash
+		var th Hash
 		if err := json.Unmarshal([]byte(strconv.Quote(tc.hash)), &th); err != nil {
 			t.Fatalf("Unmarshalling text: %v", err)
 		}
-		h := th.TransactionHash
+		h := th
 		h2 := HexToHash(tc.hash)
 
 		if h != h2 {
@@ -42,7 +42,7 @@ func TestHash(t *testing.T) {
 			t.Errorf("Hash mismatch, want: %s, got: %s", tc.hash, m)
 		}
 
-		if tc.hash != string(m2) {
+		if strconv.Quote(tc.hash) != string(m2) {
 			t.Errorf("Hash mismatch, want: %s, got: %s", tc.hash, m2)
 		}
 	}
