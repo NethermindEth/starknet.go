f46e5bbaa6584a2ec2e02ef7fd20e79e967f7031
Gregory Guillou <10611760+gregoryguillou@users.noreply.github.com>
2022-10-06 21:12:47 +0200

    add support for V1 nonce in gateway (#142)

--- gateway/account.go
+++ gateway/account.go
@@ -2,10 +2,12 @@ package gateway
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"math/big"
 	"net/http"
 	"net/url"
+	"strings"
 
 	"github.com/dontpanicdao/caigo"
 	"github.com/dontpanicdao/caigo/types"
@@ -24,6 +26,43 @@ func (sg *Gateway) AccountNonce(ctx context.Context, address string) (*big.Int, 
 	}
 
 	return caigo.HexToBN(resp[0]), nil
+}
+
+func (sg *Gateway) Nonce(ctx context.Context, contractAddress, blockHashOrTag string) (*big.Int, error) {
+
+	req, err := sg.newRequest(ctx, http.MethodGet, "/get_nonce", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	appendQueryValues(req, url.Values{
+		"contractAddress": []string{contractAddress},
+	})
+	switch {
+	case strings.HasPrefix(blockHashOrTag, "0x"):
+		appendQueryValues(req, url.Values{
+			"blockHash": []string{blockHashOrTag},
+		})
+	case blockHashOrTag == "":
+		appendQueryValues(req, url.Values{
+			"blockNumber": []string{"pending"},
+		})
+	default:
+		appendQueryValues(req, url.Values{
+			"blockNumber": []string{blockHashOrTag},
+		})
+	}
+
+	var resp string
+	err = sg.do(req, &resp)
+	if err != nil {
+		return nil, err
+	}
+	nonce, ok := big.NewInt(0).SetString(resp, 0)
+	if !ok {
+		return nil, errors.New("nonce not found")
+	}
+	return nonce, nil
 }
 
 func (sg *Gateway) EstimateFee(ctx context.Context, call types.FunctionInvoke, hash string) (*types.FeeEstimate, error) {
