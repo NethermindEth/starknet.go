509aa78e502bbc3fe91551c8fd5937c83cbd1302
Matthew Coburn <mattcoburn24@gmail.com>
2022-10-10 16:44:31 -0700

    fix the query for estimatefee on testnet

--- examples/account/main.go
+++ examples/account/main.go
@@ -11,10 +11,10 @@ import (
 )
 
 var (
-	name            string = "devnet"
-	counterContract string = "0x002bb8640f7875a6c73469149b261c99897f33aea38f272b5d8644c6c67bd278"
-	address         string = "0x036f1f6a2c7f7b5180164a7f51a7e46d63bf5b898bd5144375858d4453725309"
-	privakeKey      string = "0x2623ddff7889c38d0504dbe90d655402376601a73f7fea9864d8d302e8ba34d"
+	name            string = "testnet"
+	counterContract string = "0x0331034cbde9af8aef62929b5886b096ae3d11e33e6ca23122669e928d406500"
+	address         string = "0x126dd900b82c7fc95e8851f9c64d0600992e82657388a48d3c466553d4d9246"
+	privakeKey      string = "0x879d7dad7f9df54e1474ccf572266bba36d40e3202c799d6c477506647c126"
 	feeMargin       uint64 = 115
 	maxPoll         int    = 5
 	pollInterval    int    = 3
@@ -22,17 +22,17 @@ var (
 
 func main() {
 	// init starknet gateway client
-	gw := gateway.NewClient(gateway.WithChain(name), gateway.WithBaseURL("http://localhost:8080"))
+	gw := gateway.NewClient(gateway.WithChain(name))
 
 	// get count before tx
-	// callResp, err := gw.Call(context.Background(), types.FunctionCall{
-	// 	ContractAddress:    types.HexToHash(counterContract),
-	// 	EntryPointSelector: "get_count",
-	// }, "")
-	// if err != nil {
-	// 	panic(err.Error())
-	// }
-	// fmt.Println("Counter is currently at: ", callResp[0])
+	callResp, err := gw.Call(context.Background(), types.FunctionCall{
+		ContractAddress:    types.HexToHash(counterContract),
+		EntryPointSelector: "get_count",
+	}, "")
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("Counter is currently at: ", callResp[0])
 
 	// init account handler
 	account, err := caigo.NewGatewayAccount(privakeKey, address, gw)
@@ -58,19 +58,19 @@ func main() {
 	fmt.Printf("Fee:\n\tEstimate\t\t%v wei\n\tEstimate+Margin\t\t%v wei\n\n", feeEstimate.OverallFee, max)
 
 	// execute transaction
-	execResp, err := account.Execute(context.Background(), increment, types.ExecuteDetails{MaxFee: max})
+	execResp, err := account.Execute(context.Background(), increment, types.ExecuteDetails{MaxFee: big.NewInt(1000000000000)})
 	if err != nil {
 		panic(err.Error())
 	}
-	fmt.Printf("%+v\n", execResp)
-	n, receipt, err := gw.WaitForTransaction(context.Background(), execResp.TransactionHash, pollInterval, maxPoll)
+
+	n, receipt, err := gw.WaitForTransaction(context.Background(), execResp.TransactionHash,  pollInterval, maxPoll)
 	if err != nil {
 		panic(err.Error())
 	}
 	fmt.Printf("Poll %dsec %dx \n\ttransaction(%s) receipt: %s\n\n", n*pollInterval, n, execResp.TransactionHash, receipt.Status)
 
 	// get count after tx
-	callResp, err := gw.Call(context.Background(), types.FunctionCall{
+	callResp, err = gw.Call(context.Background(), types.FunctionCall{
 		ContractAddress:    types.HexToHash(counterContract),
 		EntryPointSelector: "get_count",
 	}, "")
