fdd1de5668c135693d78f05d54138b4d16668802
Matthew Coburn <mattcoburn24@gmail.com>
2022-10-17 14:35:38 -0700

    add working test for test_block_devnet

--- gateway/block_test.go
+++ gateway/block_test.go
@@ -3,75 +3,70 @@ package gateway
 import (
 	"context"
 	"log"
-	"net/http"
 	"testing"
 )
 
 func Test_Block_Devnet(t *testing.T) {
-    gw := NewClient()
+	gw := NewClient(WithChain("devnet"))
 
-    // I believe setup devnet is the function thats incrementing the block by 1
-    err := setupDevnet(context.Background())
+	type testSetType struct {
+		BlockNumber uint64
+	}
+
+	testSet := map[string][]testSetType{
+		"devnet": {{BlockNumber: 1}},
+	}[testEnv]
+
+	for _, test := range testSet {
+
+		err := setupDevnet(context.Background())
+
 		if err != nil {
 			log.Fatal("error starting test", err)
 		}
-    
-    // not sure if I should be using gw.Block to check for the current block number
 
-    //     block, err := gw.Block(context.Background())
-    // if block.BlockNumber != 1 {
-    //     t.Fatal("block number should be 1")
-    // }
-    // log.Println(block)
-
-    // // or be making a newRequest for the block?
-    // // Think I should make a request for the blocknumber and check if its equal to 1
-    r, err := gw.newRequest(context.Background(), http.MethodGet, "/get_block", nil)
-    if err != nil {
-        t.Fatal()
-    }
-    log.Println(r)
-    
-
+		block, _ := gw.Block(context.Background(), &BlockOptions{BlockNumber: test.BlockNumber})
+		if block.BlockNumber != int(test.BlockNumber) {
+			t.Fatal("block number should be 1")
+		}
+		if len(block.Transactions) == 0 {
+			log.Fatal("should have atleast 1 tx")
+		}
+	}
 }
 
-
 func Test_Block(t *testing.T) {
-    testConfig := beforeEach(t)
+	testConfig := beforeEach(t)
 
-    type testSetType struct {
-        BlockHash string
-        opts      *BlockOptions
-    }
-    testSet := map[string][]testSetType{
-        "devnet": {
-            // testConfig.client.do()
-            // TODO/2: check if devnet starts with 1 for instance and we can have a test
-            // If not, we would have to create a block
-        },
-        "testnet": {{
-            // TODO/3: instead of testing just the blockHash we should
-            // (1) Marshal the block back into a []byte and (2) compare it with the original message
-            // check "github.com/nsf/jsondiff" for ideas how to do that.
-            BlockHash: "0x57f5102f7c61826926a4d76e544d2272cad091aa4e4b12e8e3e2120a220bd11",
-            opts:      &BlockOptions{BlockNumber: 159179}}},
-            // TODO/1: add a test for mainnet
+	type testSetType struct {
+		BlockHash string
+		opts      *BlockOptions
+	}
+	testSet := map[string][]testSetType{
+		"devnet": {},
+		"testnet": {{
+			// TODO/3: instead of testing just the blockHash we should
+			// (1) Marshal the block back into a []byte and (2) compare it with the original message
+			// check "github.com/nsf/jsondiff" for ideas how to do that.
+			BlockHash: "0x57f5102f7c61826926a4d76e544d2272cad091aa4e4b12e8e3e2120a220bd11",
+			opts:      &BlockOptions{BlockNumber: 159179}}},
+
 		"mainnet": {{
 			BlockHash: "0x3bb30a6d1a3b6dcbc935b18c976126ab8d1fea60ef055be3c78530624824d50",
-            opts: &BlockOptions{BlockNumber: 5879},
+			opts:      &BlockOptions{BlockNumber: 5879},
 		}},
-    }[testEnv]
+	}[testEnv]
 
-    for _, test := range testSet {
-        block, err := testConfig.client.Block(context.Background(), test.opts)
+	for _, test := range testSet {
+		block, err := testConfig.client.Block(context.Background(), test.opts)
 
-        if err != nil {
-            t.Fatal(err)
-        }
-        if block.BlockHash != test.BlockHash {
-            t.Fatalf("expecting %s, instead: %s", block.BlockHash, test.BlockHash)
-        }
-    }
+		if err != nil {
+			t.Fatal(err)
+		}
+		if block.BlockHash != test.BlockHash {
+			t.Fatalf("expecting %s, instead: %s", block.BlockHash, test.BlockHash)
+		}
+	}
 }
 
 func Test_BlockIDByHash(t *testing.T) {
