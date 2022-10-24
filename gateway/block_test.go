f437d1bd5eeee2cba76f9bc02311c17ff91ae366
Coburn <mattcoburn24@gmail.com>
2022-10-24 11:31:12 -0700

    remove block hash by id and block id by hash (#169)

--- gateway/block_test.go
+++ gateway/block_test.go
@@ -68,29 +68,3 @@ func Test_Block(t *testing.T) {
 		}
 	}
 }
-
-func Test_BlockIDByHash(t *testing.T) {
-	gw := NewClient()
-
-	id, err := gw.BlockIDByHash(context.Background(), "0x5239614da0a08b53fa8cbdbdcb2d852e153027ae26a2ae3d43f7ceceb28551e")
-	if err != nil || id == 0 {
-		t.Errorf("Getting Block ID by Hash: %v", err)
-	}
-
-	if id != 159179 {
-		t.Errorf("Wrong Block ID from Hash: %v", err)
-	}
-}
-
-func Test_BlockHashByID(t *testing.T) {
-	gw := NewClient()
-
-	id, err := gw.BlockHashByID(context.Background(), 159179)
-	if err != nil || id == "" {
-		t.Errorf("Getting Block ID by Hash: %v", err)
-	}
-
-	if id != "0x5239614da0a08b53fa8cbdbdcb2d852e153027ae26a2ae3d43f7ceceb28551e" {
-		t.Errorf("Wrong Block ID from Hash: %v", err)
-	}
-}
