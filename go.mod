d515595daf95596481329f628ccfb8a40356eedb
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2022-10-31 22:43:05 +0100

    EC multiplication constant time  (#172)

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/nsf/jsondiff v0.0.0-20210926074059-1e845ec5d249
 	github.com/urfave/cli/v2 v2.3.0
 	golang.org/x/crypto v0.0.0-20220321153916-2c7772ba3064
+	gonum.org/v1/gonum v0.12.0
 )
 
 require (
@@ -20,7 +21,7 @@ require (
 	github.com/russross/blackfriday/v2 v2.0.1 // indirect
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
-	github.com/stretchr/testify v1.7.1 // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
 	github.com/tklauser/go-sysconf v0.3.5 // indirect
 	github.com/tklauser/numcpus v0.2.2 // indirect
 	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
--- utils.go
+++ utils.go
@@ -30,9 +30,19 @@ func (sc StarkCurve) GetRandomPrivateKey() (priv *big.Int, err error) {
 
 // obtain public key coordinates from stark curve given the private key
 func (sc StarkCurve) PrivateToPoint(privKey *big.Int) (x, y *big.Int, err error) {
+	return sc.privateToPoint(privKey, sc.EcMult)
+}
+
+// ec multiplication fn
+type EcMultiFn func(m, x1, y1 *big.Int) (x, y *big.Int)
+
+// obtain public key coordinates from stark curve given the private key
+// NOTICE: configurable ec multiplication fn, used for testing
+func (sc StarkCurve) privateToPoint(privKey *big.Int, ecMulti EcMultiFn) (x, y *big.Int, err error) {
 	if privKey.Cmp(big.NewInt(0)) != 1 || privKey.Cmp(sc.N) != -1 {
 		return x, y, fmt.Errorf("private key not in curve range")
 	}
-	x, y = sc.EcMult(privKey, sc.EcGenX, sc.EcGenY)
+
+	x, y = ecMulti(privKey, sc.EcGenX, sc.EcGenY)
 	return x, y, nil
 }
