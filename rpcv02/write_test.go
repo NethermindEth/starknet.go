a88651494244982e8e6e4b8ead81854021196042
gregory <10611760+gregoryguillou@users.noreply.github.com>
2022-12-08 21:58:58 +0100

    test rpcv02

--- rpcv02/write_test.go
+++ rpcv02/write_test.go
@@ -34,7 +34,7 @@ func TestDeclareTransaction(t *testing.T) {
 			Filename:          artifacts.CounterCompiled,
 			Version:           TransactionV1,
 			Signature:         []string{"0x62fe3eb3f30824c8039e5a8b9f16e02f6ad71e01d5593b330451fccd97a2576", "0x33eda2c4b010a3365a9ed453c0f21b796f84e03cc33d3602f49f57cd50d2270"},
-			ExpectedClassHash: "0x7853290c8ec2d52a9e2b241abb6d7fd0757c1c28c8be7e2471ed3357d5c0758",
+			ExpectedClassHash: "0x29c64881bf658fae000fa6d5112f379eb4fc9c629a5cd7455eafc0744e34a8a",
 		}},
 	}[testEnv]
 
@@ -69,80 +69,5 @@ func TestDeclareTransaction(t *testing.T) {
 			t.Fatal("expecting to match", err)
 		}
 		fmt.Println("transaction hash:", dec.TransactionHash)
-	}
-}
-
-// TestDeployTransaction tests starknet_addDeployTransaction
-func TestDeployTransaction(t *testing.T) {
-	testConfig := beforeEach(t)
-
-	type testSetType struct {
-		Filename                []byte
-		Salt                    string
-		ConstructorCall         []string
-		ExpectedContractAddress string
-	}
-	testSet := map[string][]testSetType{
-		"devnet": {
-			{
-				Filename:                artifacts.CounterCompiled,
-				Salt:                    "0xdeadbeef",
-				ConstructorCall:         []string{},
-				ExpectedContractAddress: "0xbaaa96effb3564b6047e45944e8db9d9b0a056886d131038baabb56a959390",
-			},
-			{
-				Filename:                artifacts.AccountV0Compiled,
-				Salt:                    "0xdeadbeef",
-				ConstructorCall:         []string{TestPublicKey},
-				ExpectedContractAddress: TestNetAccount032Address,
-			},
-			{
-				Filename:                artifacts.AccountCompiled,
-				Salt:                    "0xdeadbeef",
-				ConstructorCall:         []string{TestPublicKey},
-				ExpectedContractAddress: TestNetAccount040Address,
-			},
-		},
-		"mainnet": {},
-		"mock":    {},
-		"testnet": {
-			{
-				Filename:                artifacts.CounterCompiled,
-				Salt:                    "0xdeadbeef",
-				ConstructorCall:         []string{},
-				ExpectedContractAddress: "0x694e6ea14f47c9413e7c38ba6e2f98d21c1fc14f663e5ee05b13e350a2ba216",
-			},
-		},
-	}[testEnv]
-
-	for _, test := range testSet {
-		contractClass := types.ContractClass{}
-		if err := json.Unmarshal(test.Filename, &contractClass); err != nil {
-			t.Fatal(err)
-		}
-
-		spy := NewSpy(testConfig.provider.c)
-		testConfig.provider.c = spy
-		broadcastedDeployTransaction := BroadcastedDeployTransaction{
-			Type:                "DEPLOY",
-			Version:             big.NewInt(0),
-			ContractAddressSalt: test.Salt,
-			ConstructorCalldata: test.ConstructorCall,
-			ContractClass:       contractClass,
-		}
-		dec, err := testConfig.provider.AddDeployTransaction(context.Background(), broadcastedDeployTransaction)
-		if err != nil {
-			t.Fatal("declare should succeed, instead:", err)
-		}
-		fmt.Printf("transaction hash: %s\n", dec.TransactionHash)
-		expectedContractAddress, _ := big.NewInt(0).SetString(test.ExpectedContractAddress, 0)
-		contractAddress, _ := big.NewInt(0).SetString(dec.ContractAddress, 0)
-		if contractAddress.Cmp(expectedContractAddress) != 0 {
-			t.Fatalf("contractAddress expecting %s, current: %s", test.ExpectedContractAddress, dec.ContractAddress)
-		}
-		if diff, err := spy.Compare(dec, false); err != nil || diff != "FullMatch" {
-			spy.Compare(dec, true)
-			t.Fatal("expecting to match", err)
-		}
 	}
 }
