81b8b2cdb58a791655b18c53d9c9fcf2cb290131
rianhughes <rian.hughes@physics.ox.ac.uk>
2023-08-31 15:09:17 +0300

    wip

--- account/account_test.go
+++ account/account_test.go
@@ -74,26 +74,12 @@ func TestSign(t *testing.T) {
 	})
 }
 
-// func TestExecute(t *testing.T) {
-// 	mockCtrl := gomock.NewController(t)
-// 	t.Cleanup(mockCtrl.Finish)
-// 	mockRpcProvider := mocks.NewMockRpcProvider(mockCtrl)
+func TestExecute(t *testing.T) {
+	// mockCtrl := gomock.NewController(t)
+	// t.Cleanup(mockCtrl.Finish)
+	// mockRpcProvider := mocks.NewMockRpcProvider(mockCtrl)
 
-// 	t.Run("Test Execute", func(t *testing.T) {
-// 		expectedHash, _ := new(felt.Felt).SetString("0x0")
-// 		address := &felt.Zero
-
-// 		mockRpcProvider.EXPECT().ChainID(context.Background()).Return("SN_GOERLI", nil)
-// 		account, err := account.NewAccount(mockRpcProvider, 1, address, starknetgo.NewMemKeystore())
-// 		require.NoError(t, err, "error returned from account.NewAccount()")
-
-// 		call := rpc.FunctionCall{ContractAddress: &felt.Zero, EntryPointSelector: &felt.Zero, Calldata: []*felt.Felt{&felt.Zero}}
-// 		txDetails := rpc.TxDetails{Nonce: &felt.Zero, MaxFee: &felt.Zero}
-
-// 		mockRpcProvider.EXPECT().AddInvokeTransaction(context.Background()).Return("SN_GOERLI", nil)
-
-// 		resp, err := account.Execute(context.Background(), call, txDetails)
-// 		require.NoError(t, err, "error returned from account.Execute()")
-// 		require.Equal(t, resp.TransactionHash, expectedHash.String(), "transaction hash does not match expected")
-// 	})
-// }
+	t.Run("Test Execute", func(t *testing.T) {
+		panic("Tests for Execute need implemented")
+	})
+}
