4437f2c174f7ba20dc2ad53a6a4790e50201abcd
rianhughes <rian.hughes@physics.ox.ac.uk>
2023-09-08 11:35:12 +0300

    rpcv03 implement simulate transactions method

--- rpc/trace.go
+++ rpc/trace.go
@@ -16,3 +16,15 @@ func (provider *Provider) TransactionTrace(ctx context.Context, hash string) err
 func (provider *Provider) TraceBlockTransactions(ctx context.Context, hash string) error {
 	return ErrNotImplemented
 }
+
+// simulate a given transaction on the requested state, and generate the execution trace
+func (provider *Provider) SimulateTransactions(ctx context.Context, blockID BlockID, txns []BroadcastedTransaction, simulationFlags []SimulationFlag) ([]SimulatedTransaction, error) {
+
+	var output []SimulatedTransaction
+	if err := do(ctx, provider.c, "starknet_simulateTransactions", &output, blockID, txns, simulationFlags); err != nil {
+		return nil, tryUnwrapToRPCErr(err, ErrContractNotFound, ErrContractError, ErrBlockNotFound)
+	}
+
+	return output, nil
+
+}
--- rpc/trace_test.go
+++ rpc/trace_test.go
@@ -2,7 +2,11 @@ package rpc
 
 import (
 	"context"
+	"encoding/json"
+	"os"
 	"testing"
+
+	"github.com/test-go/testify/require"
 )
 
 // TestNotImplemented checks the method not yet implemented
@@ -32,5 +36,51 @@ func TestNotImplemented(t *testing.T) {
 		if err == nil || err.Error() != "Method Not Found" {
 			t.Fatalf("Method %s is now available, got %v\n", test.MissingMethod, err)
 		}
+	}
+}
+
+// TestSimulateTransaction tests the SimulateTransaction method
+func TestSimulateTransaction(t *testing.T) {
+	testConfig := beforeEach(t)
+
+	var simulateTxIn SimulateTransactionInput
+	var expectedResp SimulateTransactionOutput
+	if testEnv == "mainnet" {
+		simulateTxnRaw, err := os.ReadFile("./tests/simulateInvokeTx.json")
+		require.NoError(t, err, "Error ReadFile simulateInvokeTx")
+
+		err = json.Unmarshal(simulateTxnRaw, &simulateTxIn)
+		require.NoError(t, err, "Error unmarshalling simulateInvokeTx")
+
+		expectedrespRaw, err := os.ReadFile("./tests/simulateInvokeTxResp.json")
+		require.NoError(t, err, "Error ReadFile simulateInvokeTxResp")
+
+		err = json.Unmarshal(expectedrespRaw, &expectedResp)
+		require.NoError(t, err, "Error unmarshalling simulateInvokeTxResp")
+	}
+
+	type testSetType struct {
+		SimulateTxnInput SimulateTransactionInput
+		ExpectedResp     SimulateTransactionOutput
+	}
+	testSet := map[string][]testSetType{
+		"devnet":  {},
+		"mock":    {},
+		"testnet": {},
+		"mainnet": {testSetType{
+			SimulateTxnInput: simulateTxIn,
+			ExpectedResp:     expectedResp,
+		}},
+	}[testEnv]
+
+	for _, test := range testSet {
+
+		resp, err := testConfig.provider.SimulateTransactions(
+			context.Background(),
+			test.SimulateTxnInput.BlockID,
+			test.SimulateTxnInput.Txns,
+			test.SimulateTxnInput.SimulationFlags)
+		require.NoError(t, err)
+		require.Equal(t, test.ExpectedResp.Txns, resp)
 	}
 }
