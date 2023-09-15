5e2146536bf511e5ec72933fcc44b187204c6b00
rianhughes <rian.hughes@physics.ox.ac.uk>
2023-09-15 15:48:02 +0300

    tidy

--- account/account_test.go
+++ account/account_test.go
@@ -2,7 +2,6 @@ package account_test
 
 import (
 	"context"
-	"encoding/json"
 	"flag"
 	"fmt"
 	"math/big"
@@ -168,7 +167,7 @@ func TestFmtCallData(t *testing.T) {
 			utils.TestHexToFelt(t, "0x0"),
 		}
 		fmt.Println("fnCall.asd", fnCall.EntryPointSelector)
-		fmtCallData := account.FmtCalldata2([]rpc.FunctionCall{fnCall})
+		fmtCallData := account.FmtCalldata([]rpc.FunctionCall{fnCall})
 		fmt.Println("fmtCallData", fmtCallData)
 		require.Equal(t, fmtCallData, expectedCallData)
 	})
@@ -216,31 +215,6 @@ func TestSign(t *testing.T) {
 	t.Cleanup(mockCtrl.Finish)
 	mockRpcProvider := mocks.NewMockRpcProvider(mockCtrl)
 
-	// Accepted on testnet https://goerli.voyager.online/tx/0x2a7eec54aab835323a810e893354368a496f1a217e8b6ef295476568ef08f0d
-	t.Run("Sign testnet - mock", func(t *testing.T) {
-		expectedS1 := utils.TestHexToFelt(t, "0x6bf7980d98fa300ed9565b8cd5efcf5582133daa961b5e1d9477bf1bd750727")
-		expectedS2 := utils.TestHexToFelt(t, "0x5886b8236b7dc3665c0014876a644ddd0800a167ff1036fb82af1a6f4134c91")
-
-		address := utils.TestHexToFelt(t, "0x476466998f22e0b0177ddc76afcf8e3b5d30164f3eb33031aae7a9cb63c831")
-		// pubKey := utils.TestHexToFelt(t, "0xc1e5fc3f93e04dac29878e4efccd81a96547884628d83b40fc9b758b5a349")
-		privKey := utils.TestHexToFelt(t, "0x15d0b81e6140f4cce02b47609879a723f9f5b7b9f3ffca346018c73fe81847e")
-		privKeyBI, ok := new(big.Int).SetString(privKey.String(), 0)
-		require.True(t, ok)
-		ks := starknetgo.NewMemKeystore()
-		ks.Put(address.String(), privKeyBI)
-
-		mockRpcProvider.EXPECT().ChainID(context.Background()).Return("SN_GOERLI", nil)
-		account, err := account.NewAccount(mockRpcProvider, 1, address, address.String(), ks)
-		require.NoError(t, err, "error returned from account.NewAccount()")
-
-		msg := utils.TestHexToFelt(t, "0x2a7eec54aab835323a810e893354368a496f1a217e8b6ef295476568ef08f0d")
-		sig, err := account.Sign(context.Background(), msg)
-
-		require.NoError(t, err, "error returned from account.Sign()")
-		require.Equal(t, expectedS1.String(), sig[0].String(), "s1 does not match expected")
-		require.Equal(t, expectedS2.String(), sig[1].String(), "s2 does not match expected")
-	})
-
 	// Accepted on testnet https://goerli.voyager.online/tx/0x73cf79c4bfa0c7a41f473c07e1be5ac25faa7c2fdf9edcbd12c1438f40f13d8
 	t.Run("Sign testnet - mock 2", func(t *testing.T) {
 		expectedS1 := utils.TestHexToFelt(t, "0x10d405427040655f118bc8b897e2f2f8147858bbcb0e3d6bc6dfbc6d0205e8")
@@ -273,16 +247,6 @@ func TestAddInvoke(t *testing.T) {
 		if testEnv != "testnet" {
 			t.Skip("Skipping test as it requires a testnet environment")
 		}
-		// Why does deploying an account work (which reqs a sig), but this doesnt?
-		// Should use another library and force it to print data out to see what's going wrong here
-		// New Client
-		fmt.Println("base", base)
-
-		fmt.Println("base", new(felt.Felt).SetBytes([]byte(account.TRANSACTION_PREFIX)))
-		fmt.Println("base", types.UTF8StrToBig(starknetgo.TRANSACTION_PREFIX))
-		bigg := types.UTF8StrToBig(starknetgo.TRANSACTION_PREFIX)
-		fellt := new(felt.Felt).SetBytes(bigg.Bytes())
-		fmt.Println("base", fellt)
 		client, err := rpc.NewClient(base + "/rpc")
 		require.NoError(t, err, "Error in rpc.NewClient")
 		provider := rpc.NewProvider(client)
@@ -302,18 +266,16 @@ func TestAddInvoke(t *testing.T) {
 		require.NoError(t, err)
 
 		// Now build the trasaction
-		nonce, _ := new(felt.Felt).SetString("0x3") // should be 0x2 for the requries
-		mxfee, _ := new(felt.Felt).SetString("0x574fbde6000")
+		maxFee, _ := new(felt.Felt).SetString("0x574fbde6000")
 		invokeTx := rpc.BroadcastedInvokeV1Transaction{
 			BroadcastedTxnCommonProperties: rpc.BroadcastedTxnCommonProperties{
-				Nonce:   nonce,
-				MaxFee:  mxfee,
+				Nonce:   new(felt.Felt).SetUint64(3),
+				MaxFee:  maxFee,
 				Version: rpc.TransactionV1,
 				Type:    rpc.TransactionType_Invoke,
 			},
 			SenderAddress: acnt.AccountAddress,
 		}
-		fmt.Println(" ====== ======invokeTx", invokeTx)
 		fnCall := rpc.FunctionCall{
 			ContractAddress:    utils.TestHexToFelt(t, "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7"),
 			EntryPointSelector: types.GetSelectorFromNameFelt("transfer"),
@@ -322,59 +284,20 @@ func TestAddInvoke(t *testing.T) {
 				utils.TestHexToFelt(t, "0x1"),
 			},
 		}
-		// fmt.Println(fnCall.EntryPointSelector)
-		// err = account.BuildInvokeTx(context.Background(), &invokeTx, &[]rpc.FunctionCall{fnCall})
-		// require.NoError(t, err, "Error in BuildInvokeTx")
-
-		fmtdCallData := account.FmtCalldata2([]rpc.FunctionCall{fnCall})
-		invokeTx.Calldata = fmtdCallData
+		invokeTx.Calldata = account.FmtCalldata([]rpc.FunctionCall{fnCall})
 
 		/// NEED TO FORMAT THE CALL DATA
-		fmt.Println("+++++++ pre TransactionHash2", invokeTx.Nonce, invokeTx.MaxFee)
 		txHash, err := acnt.TransactionHash2(invokeTx.Calldata, invokeTx.Nonce, invokeTx.MaxFee, acnt.AccountAddress)
-		// require.Equal(t, txHash.String(), "0x73cf79c4bfa0c7a41f473c07e1be5ac25faa7c2fdf9edcbd12c1438f40f13d8")
 		x, y, err := starknetgo.Curve.SignFelt(txHash, fakePrivKeyFelt)
-		if err != nil {
-			panic(err)
-		}
-		// require.Equal(t, x.String(), "0x10d405427040655f118bc8b897e2f2f8147858bbcb0e3d6bc6dfbc6d0205e8")
-		// require.Equal(t, y.String(), "0x5cdfe4a3d5b63002e9011ec0ba59ae2b75a43cb2a3bc1699b35aa64cb9ca3cf")
-
-		fmt.Println("+++++++ post TransactionHash2", invokeTx.Nonce, invokeTx.MaxFee)
-		invokeTx.Signature = []*felt.Felt{x, y}
-		fmt.Println(" ====== invokeTx", invokeTx)
-		// fmt.Println("sig", invokeTx.Signature)
-		// fmt.Println("invokeTx.Calldata", invokeTx.Calldata)
-		// fmt.Println("txHash", txHash)
-		// fmt.Println(x, y)
-		fmt.Println(" ====== invokeTx", invokeTx)
-		qwe, _ := json.MarshalIndent(invokeTx, "", "")
-		fmt.Println("MarshalIndent", string(qwe))
-		fmt.Println("+++++++ post MarshalIndent", invokeTx.Nonce, invokeTx.MaxFee)
-		fmt.Println(" ====== invokeTx", invokeTx)
-
-		///
-		// pub, _ := new(big.Int).SetString("2090221843434510384432085791482977629840322403554658343615172301617258923551", 0)
-		// hash, _ := new(big.Int).SetString("2122438891878094235855424351251599998644607787805056148746903357402852875679", 0)
-		// fmt.Println("")
-		// fmt.Println("txHash", txHash) // It seems the txhash is incorrect. Signature is correct.
-		// fmt.Println("acntadr", accountAddress, accountAddress.BigInt(new(big.Int)))
-		// fmt.Println("pub", pub, new(felt.Felt).SetBytes(pub.Bytes()))
-		// fmt.Println("hash", hash, new(felt.Felt).SetBytes(hash.Bytes()))
-		// fmt.Println("")
-		// fmt.Println(invokeTx.Signature[0].BigInt(new(big.Int)))
-		// fmt.Println(invokeTx.Signature[1].BigInt(new(big.Int)))
-		// fmt.Println(invokeTx.Signature[0].BigInt(new(big.Int)))
-		// fmt.Println(invokeTx.Signature[1].BigInt(new(big.Int)))
-		// // Send tx
-		resp, err := acnt.AddInvokeTransaction(context.Background(), &invokeTx)
-		fmt.Println("resp", resp, err)
 		require.NoError(t, err)
+		invokeTx.Signature = []*felt.Felt{x, y}
+		resp, err := acnt.AddInvokeTransaction(context.Background(), &invokeTx)
+		require.NoError(t, err)
+		require.NotNil(t, resp)
 	})
 }
 
 func newDevnet(t *testing.T, url string) ([]test.TestAccount, error) {
-	// url := SetupLocalStarknetNode(t)
 	devnet := test.NewDevNet(url)
 	acnts, err := devnet.Accounts()
 	return acnts, err
