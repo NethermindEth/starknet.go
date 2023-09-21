0698bbe9131414d3d687da091bb41411a66b9e9d
Bitcoinnoobie <109748565+Bitcoinnoobie@users.noreply.github.com>
2023-09-22 02:44:22 +0530

    Update events.go

--- rpc/events.go
+++ rpc/events.go
@@ -9,17 +9,8 @@ import (
 func (provider *Provider) Events(ctx context.Context, input EventsInput) (*EventChunk, error) {
 	var result EventChunk
 	if err := do(ctx, provider.c, "starknet_getEvents", &result, input); err != nil {
-		switch {
-		case errors.Is(err, ErrPageSizeTooBig):
-			return nil, ErrPageSizeTooBig
-		case errors.Is(err, ErrInvalidContinuationToken):
-			return nil, ErrInvalidContinuationToken
-		case errors.Is(err, ErrBlockNotFound):
-			return nil, ErrBlockNotFound
-		case errors.Is(err, ErrTooManyKeysInFilter):
-			return nil, ErrTooManyKeysInFilter
-		}
-		return nil, err
+
+		return nil, tryUnwrapToRPCErr(err,  ErrPageSizeTooBig , ErrInvalidContinuationToken , ErrBlockNotFound ,ErrTooManyKeysInFilter)
 	}
 	return &result, nil
 }
