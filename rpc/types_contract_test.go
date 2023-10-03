43c6afc798f1fc2f9dc1866af0b666664eaaaee9
Carmen Cabrera <kr1000a@gmail.com>
2023-10-03 08:59:50 -0400

    rename folder for tests

--- rpc/types_contract_test.go
+++ rpc/types_contract_test.go
@@ -7,8 +7,8 @@ import (
 )
 
 const (
-	validDeprecatedContractCompiledPath = "./tests/types_contract_test/0x1efa8f84fd4dff9e2902ec88717cf0dafc8c188f80c3450615944a469428f7f.json"
-	validContractCompiledPath           = "./tests/types_contract_test/0x03a8Bad0A71696fC3eB663D0513Dc165Bb42cD4b662e633e3F87a49627CF3AEF.json"
+	validDeprecatedContractCompiledPath = "./tests/contract/0x1efa8f84fd4dff9e2902ec88717cf0dafc8c188f80c3450615944a469428f7f.json"
+	validContractCompiledPath           = "./tests/contract/0x03a8Bad0A71696fC3eB663D0513Dc165Bb42cD4b662e633e3F87a49627CF3AEF.json"
 	invalidContractCompiledPath         = "./tests/0xFakeContract.json"
 )
 
