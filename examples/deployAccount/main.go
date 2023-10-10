a3676e6dd9c8d858f25efc8b6ba5c36bd7b74162
Rian Hughes <ryanhughes4500@hotmail.com>
2023-10-10 17:51:54 +0100

    Update examples to use account interface (#414)
    * Update examples to use account interface
    
    * Add .env.template to deployAccount example
    
    ---------
    
    Co-authored-by: Carmen Cabrera <kr1000a@gmail.com>

--- examples/deployAccount/main.go
+++ examples/deployAccount/main.go
@@ -6,7 +6,7 @@ import (
 	"os"
 
 	"github.com/NethermindEth/juno/core/felt"
-	"github.com/NethermindEth/starknet.go/curve"
+	"github.com/NethermindEth/starknet.go/account"
 	"github.com/NethermindEth/starknet.go/rpc"
 	"github.com/NethermindEth/starknet.go/utils"
 	ethrpc "github.com/ethereum/go-ethereum/rpc"
@@ -16,6 +16,7 @@ import (
 var (
 	network              string = "testnet"
 	predeployedClassHash        = "0x2794ce20e5f2ff0d40e632cb53845b9f4e526ebd8471983f7dbd355b721d5a"
+	accountAddress              = "0xdeadbeef"
 )
 
 func main() {
@@ -28,14 +29,26 @@ func main() {
 	}
 	clientv02 := rpc.NewProvider(c)
 
-	// Get keys
-	pub, priv := getRandomKeys()
+	// Get random keys for test purposes
+	ks, pub, _ := account.GetRandomKeys()
+
+	accountAddressFelt, err := new(felt.Felt).SetString(accountAddress)
+	if err != nil {
+		panic("Error casting accountAddress to felt")
+	}
+
+	// Set up account
+	acnt, err := account.NewAccount(clientv02, accountAddressFelt, pub.String(), ks)
+	if err != nil {
+		panic(err)
+	}
 
 	classHash, err := utils.HexToFelt(predeployedClassHash)
 	if err != nil {
 		panic(err)
 	}
 
+	// Create transaction data
 	tx := rpc.DeployAccountTxn{
 		Nonce:               &felt.Zero, // Contract accounts start with nonce zero.
 		MaxFee:              new(felt.Felt).SetUint64(4724395326064),
@@ -47,7 +60,7 @@ func main() {
 		ConstructorCalldata: []*felt.Felt{pub},
 	}
 
-	precomputedAddress, err := precomputeAddress(&felt.Zero, pub, classHash, tx.ConstructorCalldata)
+	precomputedAddress, err := acnt.PrecomputeAddress(&felt.Zero, pub, classHash, tx.ConstructorCalldata)
 	fmt.Println("precomputedAddress:", precomputedAddress)
 
 	// At this point you need to add funds to precomputed address to use it.
@@ -58,133 +71,16 @@ func main() {
 	fmt.Println("When your account has been funded by the faucet, press any key, then `enter` to continue : ")
 	fmt.Scan(&input)
 
-	// Get the chainID to sign the transaction
-	chainId, err := clientv02.ChainID(context.Background())
+	// Sign the transaction
+	err = acnt.SignDeployAccountTransaction(context.Background(), &tx, precomputedAddress)
 	if err != nil {
 		panic(err)
 	}
-
-	// Calculate and sign the transaction hash
-	hash, err := calculateDeployAccountTransactionHash(tx, precomputedAddress, chainId)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Transaction hash:", hash)
-	x, y, err := curve.Curve.SignFelt(hash, priv)
-	if err != nil {
-		panic(err)
-	}
-	tx.Signature = []*felt.Felt{x, y}
 
 	// Send transaction to the network
-	resp, err := clientv02.AddDeployAccountTransaction(context.Background(), tx)
+	resp, err := acnt.AddDeployAccountTransaction(context.Background(), tx)
 	if err != nil {
 		panic(fmt.Sprint("Error returned from AddDeployAccountTransaction: ", err))
 	}
-
 	fmt.Println("AddDeployAccountTransaction response:", resp)
-
-}
-
-func getRandomKeys() (*felt.Felt, *felt.Felt) {
-	privateKey, err := curve.Curve.GetRandomPrivateKey()
-	if err != nil {
-		fmt.Println("can't get random private key:", err)
-		os.Exit(1)
-	}
-	pubX, _, err := curve.Curve.PrivateToPoint(privateKey)
-	if err != nil {
-		fmt.Println("can't generate public key:", err)
-		os.Exit(1)
-	}
-	privFelt := utils.BigIntToFelt(privateKey)
-	pubFelt := utils.BigIntToFelt(pubX)
-	
-	return pubFelt, privFelt
-}
-
-// precomputeAddress computes the address by hashing the relevant data.
-// ref: https://github.com/starkware-libs/cairo-lang/blob/master/src/starkware/starknet/core/os/contract_address/contract_address.py
-// TODO: Move to contract / utils package
-func precomputeAddress(deployerAddress *felt.Felt, salt *felt.Felt, classHash *felt.Felt, constructorCalldata []*felt.Felt) (*felt.Felt, error) {
-	CONTRACT_ADDRESS_PREFIX := new(felt.Felt).SetBytes([]byte("STARKNET_CONTRACT_ADDRESS"))
-
-	bigIntArr := utils.FeltArrToBigIntArr([]*felt.Felt{
-		CONTRACT_ADDRESS_PREFIX,
-		deployerAddress,
-		salt,
-		classHash,
-	})
-	constructorCalldataBigIntArr := utils.FeltArrToBigIntArr(constructorCalldata)
-	constructorCallDataHashInt, _ := curve.Curve.ComputeHashOnElements(constructorCalldataBigIntArr)
-	bigIntArr = append(bigIntArr, constructorCallDataHashInt)
-
-	preBigInt, err := curve.Curve.ComputeHashOnElements(bigIntArr)
-	if err != nil {
-		return nil, err
-	}
-	return utils.BigIntToFelt(preBigInt), nil
-
-}
-
-func computeHashOnElementsFelt(feltArr []*felt.Felt) (*felt.Felt, error) {
-	bigIntArr := utils.FeltArrToBigIntArr(feltArr)
-	hash, err := curve.Curve.ComputeHashOnElements(bigIntArr)
-	if err != nil {
-		return nil, err
-	}
-	return utils.BigIntToFelt(hash), nil
-}
-
-// calculateDeployAccountTransactionHash computes the transaction hash for deployAccount transactions
-func calculateDeployAccountTransactionHash(tx rpc.DeployAccountTxn, contractAddress *felt.Felt, chainID string) (*felt.Felt, error) {
-	Prefix_DEPLOY_ACCOUNT := new(felt.Felt).SetBytes([]byte("deploy_account"))
-	chainIdFelt := new(felt.Felt).SetBytes([]byte(chainID))
-
-	calldata := []*felt.Felt{tx.ClassHash, tx.ContractAddressSalt}
-	calldata = append(calldata, tx.ConstructorCalldata...)
-	calldataHash, err := computeHashOnElementsFelt(calldata)
-	if err != nil {
-		return nil, err
-	}
-
-	versionFelt, err := utils.HexToFelt(string(tx.Version))
-	if err != nil {
-		return nil, err
-	}
-
-	return calculateTransactionHashCommon(
-		Prefix_DEPLOY_ACCOUNT,
-		versionFelt,
-		contractAddress,
-		&felt.Zero,
-		calldataHash,
-		tx.MaxFee,
-		chainIdFelt,
-		[]*felt.Felt{tx.Nonce},
-	)
-}
-
-func calculateTransactionHashCommon(
-	txHashPrefix *felt.Felt,
-	version *felt.Felt,
-	contractAddress *felt.Felt,
-	entryPointSelector *felt.Felt,
-	calldata *felt.Felt,
-	maxFee *felt.Felt,
-	chainId *felt.Felt,
-	additionalData []*felt.Felt) (*felt.Felt, error) {
-
-	dataToHash := []*felt.Felt{
-		txHashPrefix,
-		version,
-		contractAddress,
-		entryPointSelector,
-		calldata,
-		maxFee,
-		chainId,
-	}
-	dataToHash = append(dataToHash, additionalData...)
-
-	return computeHashOnElementsFelt(dataToHash)
 }
