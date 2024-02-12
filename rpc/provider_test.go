5d8c61059a5843afde544aa9e4d09eb2f732c82d
Rian Hughes <ryanhughes4500@hotmail.com>
2024-02-12 11:10:08 +0200

    update rpc provider to use cookies (#522)
    * update rpc provider to use cookies

--- rpc/provider_test.go
+++ rpc/provider_test.go
@@ -2,14 +2,16 @@ package rpc
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"math/big"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"strings"
 	"testing"
 
-	ethrpc "github.com/ethereum/go-ethereum/rpc"
 	"github.com/joho/godotenv"
 	"github.com/test-go/testify/require"
 )
@@ -102,12 +104,12 @@ func beforeEach(t *testing.T) *testConfiguration {
 	if base != "" {
 		testConfig.base = base
 	}
-	c, err := ethrpc.DialContext(context.Background(), testConfig.base)
+	c, err := NewProvider(testConfig.base)
 	if err != nil {
 		t.Fatal("connect should succeed, instead:", err)
 	}
-	client := NewProvider(c)
-	testConfig.provider = client
+
+	testConfig.provider = c
 	t.Cleanup(func() {
 		testConfig.provider.c.Close()
 	})
@@ -211,4 +213,45 @@ func TestSyncing(t *testing.T) {
 		}
 
 	}
+}
+
+func TestCookieManagement(t *testing.T) {
+	// Don't return anything unless cookie is set.
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if _, err := r.Cookie("session_id"); err == http.ErrNoCookie {
+			http.SetCookie(w, &http.Cookie{
+				Name:  "session_id",
+				Value: "12345",
+				Path:  "/",
+			})
+		} else {
+			var result string
+			err := mock_starknet_chainId(&result, "")
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			json.NewEncoder(w).Encode(map[string]interface{}{
+				"jsonrpc": "2.0",
+				"id":      1,
+				"result":  result,
+			})
+		}
+	}))
+	defer server.Close()
+
+	client, err := NewProvider(server.URL)
+	require.Nil(t, err)
+
+	resp, err := client.ChainID(context.Background())
+	require.NotNil(t, err)
+	require.Equal(t, resp, "")
+
+	resp, err = client.ChainID(context.Background())
+	require.Nil(t, err)
+	require.Equal(t, resp, "SN_GOERLI")
+
+	resp, err = client.ChainID(context.Background())
+	require.Nil(t, err)
+	require.Equal(t, resp, "SN_GOERLI")
 }
