f3f4b68c4ebaf5d65757dab2c5d7191fb19944f3
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-07-03 16:39:54 +0530

    fix::> Added DeclareTx inside BroadcastDeclareTxn for Version 3 Only

--- rpc/types_broadcast_transaction.go
+++ rpc/types_broadcast_transaction.go
@@ -85,23 +85,8 @@ func (tx BroadcastDeclareTxnV2) GetContractClass() interface{} {
 }
 
 type BroadcastDeclareTxnV3 struct {
-	Type              TransactionType       `json:"type"`
-	SenderAddress     *felt.Felt            `json:"sender_address"`
-	CompiledClassHash *felt.Felt            `json:"compiled_class_hash"`
-	Version           TransactionVersion    `json:"version"`
-	Signature         []*felt.Felt          `json:"signature"`
-	Nonce             *felt.Felt            `json:"nonce"`
-	ContractClass     *ContractClass        `json:"contract_class"`
-	ResourceBounds    ResourceBoundsMapping `json:"resource_bounds"`
-	Tip               U64                   `json:"tip"`
-	// The data needed to allow the paymaster to pay for the transaction in native tokens
-	PayMasterData []*felt.Felt `json:"paymaster_data"`
-	// The data needed to deploy the account contract from which this tx will be initiated
-	AccountDeploymentData *felt.Felt `json:"account_deployment_data"`
-	// The storage domain of the account's nonce (an account has a nonce per DA mode)
-	NonceDataMode DataAvailabilityMode `json:"nonce_data_availability_mode"`
-	// The storage domain of the account's balance from which fee will be charged
-	FeeMode DataAvailabilityMode `json:"fee_data_availability_mode"`
+	DeclareTxnV3
+	ContractClass *ContractClass `json:"contract_class"`
 }
 
 func (tx BroadcastDeclareTxnV3) GetContractClass() interface{} {
