b91bf497129d6206f3e29a29274631e8581dca80
Alex Sumner <alex.sumner@gmail.com>
2024-07-11 13:39:56 +0100

    final example for event log reading

--- examples/readEvents/main.go
+++ examples/readEvents/main.go
@@ -11,14 +11,22 @@ import (
 	setup "github.com/NethermindEth/starknet.go/examples/internal"
 )
 
+const (
+	CONTRACT_ADDRESS = "0x059a943ca214c10234b9a3b61c558ac20c005127d183b86a99a8f3c60a08b4ff" 
+)
+
+var (
+	FROM_BLOCK uint64 = 657100
+	TO_BLOCK   uint64 = 657101
+)
+
 // main entry point of the program.
 //
 // It initializes the environment and establishes a connection with the client.
-// It then retrieves events from the contract and prints how many it found with 
-// a series of more selective filters (all event types, just 2 event types, just 
+// It then retrieves events from the contract and prints how many it found with
+// a series of more selective filters (all event types, just 2 event types, just
 // those 2 event types but with a specified key value).
 
-//
 // Parameters:
 //
 //	none
@@ -42,24 +50,26 @@ func main() {
 
 	// contractAddress is the address of the token contract whose events we want to read
 	// it is read from the .env file
-	contractAddrStr := setup.GetContractAddress()
-	contractAddress, err := utils.HexToFelt(contractAddrStr)
+
+	contractAddress, err := utils.HexToFelt(CONTRACT_ADDRESS)
 	if err != nil {
-		msg := fmt.Errorf("failed to transform the token contract address %s, error %w", contractAddrStr, err)
+		msg := fmt.Errorf("failed to transform the token contract address %s, error %w", CONTRACT_ADDRESS, err)
 		panic(msg)
 	}
 
 	// fromBlock and toBlock are the block numbers between which we want to read events
-	// they are read from the .env file
-	fromBlock, toBlock := setup.GetFromAndToBlocks()
+	fromBlock := &FROM_BLOCK
+	toBlock := &TO_BLOCK
 
 	// create an EventFilter that specifies which events we want
 	eventFilter := rpc.EventFilter{
-		FromBlock: rpc.BlockID{Number: &fromBlock},
-		ToBlock:   rpc.BlockID{Number: &toBlock},
+		FromBlock: rpc.BlockID{Number: fromBlock},
+		ToBlock:   rpc.BlockID{Number: toBlock},
 		Address:   contractAddress,
 	}
-	// ChunkSize is the max number of events to read in one call
+	// ChunkSize is the maximum number of events to read in one call.
+	// The readEvents function will make multiple calls to the provider
+	// when the number of matching events is larger than ChunkSize
 	resPageReq := rpc.ResultPageRequest{
 		ChunkSize: 1000,
 	}
@@ -68,9 +78,6 @@ func main() {
 		ResultPageRequest: resPageReq,
 	}
 
-	var eventTypes []string
-	eventData := [][]*felt.Felt{}
-	var keyFilter [][]*felt.Felt
 	var events []rpc.EmittedEvent
 
 	// read all the events emitted by the contract in this range of blocks
@@ -78,11 +85,14 @@ func main() {
 	fmt.Printf("num events from contract %d\n", len(events))
 
 	// narrow the scope to event types InterestStateSetBorrowingRate and InterestStateUpdated
-	eventTypes = []string{"InterestStateSetBorrowingRate", "InterestStateUpdated"}
+	eventTypes := []string{"InterestStateSetBorrowingRate", "InterestStateUpdated"}
 	// eventData is an empty slice, so we are not filtering by any key data, just event type
-	keyFilter = buildKeyFilter(eventTypes, eventData)
+	eventData := [][]*felt.Felt{}
+	keyFilter := buildKeyFilter(eventTypes, eventData)
 	// set the filter on the Keys field of the eventsInput
 	eventsInput.Keys = keyFilter
+
+	// read the events with the new filter
 	events = readEvents(eventsInput, provider)
 	fmt.Printf("num events of specified types %d\n", len(events))
 
@@ -91,6 +101,8 @@ func main() {
 	keyFilter = buildKeyFilter(eventTypes, [][]*felt.Felt{{data}})
 	// set the new filter on the Keys field of the eventsInput
 	eventsInput.Keys = keyFilter
+
+	// read the events with this filter
 	events = readEvents(eventsInput, provider)
 	fmt.Printf("num events of specified types with given keys %d\n", len(events))
 }
