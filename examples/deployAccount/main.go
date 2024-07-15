0de86de28a099f94906551b36072b0457159ff1c
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-07-15 11:13:19 +0530

    fix::> examples error solved

--- examples/deployAccount/main.go
+++ examples/deployAccount/main.go
@@ -122,7 +122,7 @@ func main() {
 
 	// Send transaction to the network
 	// resp, err := accnt.AddDeployAccountTransaction(context.Background(), tx)
-	resp, err := accnt.AddTransaction(context.Background(), tx)
+	resp, err := accnt.SendTransaction(context.Background(), tx)
 	if err != nil {
 		fmt.Println("Error returned from AddDeployAccountTransaction: ")
 		setup.PanicRPC(err)
