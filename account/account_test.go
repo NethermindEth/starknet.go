0e9f79e81413cc2cfec6565058ba1b0c8dee7cd4
thiagodeev <thiagodeev@gmail.com>
2024-11-06 15:29:29 -0300

    Fixes failing tests and improves Error() resp

--- account/account_test.go
+++ account/account_test.go
@@ -1067,7 +1067,7 @@ func TestWaitForTransactionReceipt(t *testing.T) {
 	type testSetType struct {
 		Timeout         int
 		Hash            *felt.Felt
-		ExpectedErr     error
+		ExpectedErr     *rpc.RPCError
 		ExpectedReceipt rpc.TransactionReceipt
 	}
 	testSet := map[string][]testSetType{
@@ -1087,7 +1087,10 @@ func TestWaitForTransactionReceipt(t *testing.T) {
 
 		resp, err := acnt.WaitForTransactionReceipt(ctx, test.Hash, 1*time.Second)
 		if test.ExpectedErr != nil {
-			require.Equal(t, test.ExpectedErr.Error(), err.Error())
+			rpcErr, ok := err.(*rpc.RPCError)
+			require.True(t, ok)
+			require.Equal(t, test.ExpectedErr.Code, rpcErr.Code)
+			require.Equal(t, test.ExpectedErr.Message, rpcErr.Message)
 		} else {
 			require.Equal(t, test.ExpectedReceipt.ExecutionStatus, (*resp).ExecutionStatus)
 		}
--- rpc/write_test.go
+++ rpc/write_test.go
@@ -2,7 +2,6 @@ package rpc
 
 import (
 	"context"
-	"errors"
 	"testing"
 
 	"github.com/NethermindEth/juno/core/felt"
@@ -17,7 +16,7 @@ func TestDeclareTransaction(t *testing.T) {
 	type testSetType struct {
 		DeclareTx     BroadcastDeclareTxnType
 		ExpectedResp  AddDeclareTransactionResponse
-		ExpectedError error
+		ExpectedError *RPCError
 	}
 	testSet := map[string][]testSetType{
 		"devnet":  {},
@@ -40,7 +39,7 @@ func TestDeclareTransaction(t *testing.T) {
 			DeclareTx: BroadcastDeclareTxnV1{},
 			ExpectedResp: AddDeclareTransactionResponse{
 				TransactionHash: utils.TestHexToFelt(t, "0x55b094dc5c84c2042e067824f82da90988674314d37e45cb0032aca33d6e0b9")},
-			ExpectedError: errors.New("Invalid Params"),
+			ExpectedError: &RPCError{Code: InvalidParams, Message: "Invalid Params"},
 		},
 		},
 	}[testEnv]
@@ -48,7 +47,10 @@ func TestDeclareTransaction(t *testing.T) {
 	for _, test := range testSet {
 		resp, err := testConfig.provider.AddDeclareTransaction(context.Background(), test.DeclareTx)
 		if err != nil {
-			require.Equal(t, test.ExpectedError.Error(), err.Error())
+			rpcErr, ok := err.(*RPCError)
+			require.True(t, ok)
+			require.Equal(t, test.ExpectedError.Code, rpcErr.Code)
+			require.Equal(t, test.ExpectedError.Message, rpcErr.Message)
 		} else {
 			require.Equal(t, (*resp.TransactionHash).String(), (*test.ExpectedResp.TransactionHash).String())
 		}
