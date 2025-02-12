04a5ecb7c87951948f557224029e3de72735a8f5
thiagodeev <thiagodeev@gmail.com>
2025-02-11 22:51:58 -0300

    Create UnmarshallFileToType func to unmarshal json to types

--- utils/Test.go
+++ utils/Test.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"encoding/json"
+	"os"
 	"testing"
 
 	"github.com/NethermindEth/juno/core/felt"
@@ -36,4 +38,40 @@ func TestHexArrToFelt(t testing.TB, hexArr []string) []*felt.Felt {
 	feltArr, err := HexArrToFelt(hexArr)
 	require.NoError(t, err)
 	return feltArr
+}
+
+// UnmarshallFileToType reads a JSON file at the given path and unmarshals it into the specified type T.
+// If any error occurs during file reading or unmarshalling, it fails the test.
+//
+// Parameters:
+// - t: testing.TB interface for test logging and error reporting
+// - filePath: string path to the JSON file
+// - isRPCResp: boolean indicating if the JSON file is in JSON-RPC response format
+// Returns:
+// - T: the unmarshalled data of type T
+func UnmarshallFileToType[T any](t testing.TB, filePath string, isRPCResp bool) *T {
+	t.Helper()
+	var result T
+
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		t.Fatalf("failed to read file %s: %v", filePath, err)
+	}
+
+	if isRPCResp {
+		var rpcResponse struct {
+			Result json.RawMessage `json:"result"`
+		}
+		if err := json.Unmarshal(data, &rpcResponse); err != nil {
+			t.Fatalf("failed to unmarshal RPC response from file %s: %v", filePath, err)
+		}
+		data = rpcResponse.Result
+	}
+
+	err = json.Unmarshal(data, &result)
+	if err != nil {
+		t.Fatalf("failed to unmarshal file %s: %v", filePath, err)
+	}
+
+	return &result
 }
