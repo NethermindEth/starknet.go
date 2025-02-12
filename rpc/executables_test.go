fd6e878e9d1d96c2b781ead3df0276b4319fdbbc
thiagodeev <thiagodeev@gmail.com>
2025-02-11 22:53:12 -0300

    Draft TestCompiledCasm test

--- rpc/executables_test.go
+++ rpc/executables_test.go
@@ -1,7 +1,41 @@
 package rpc
 
-import "testing"
+import (
+	"context"
+	"testing"
+
+	"github.com/NethermindEth/juno/core/felt"
+	"github.com/NethermindEth/starknet.go/utils"
+	"github.com/stretchr/testify/require"
+)
 
 func TestCompiledCasm(t *testing.T) {
-	t.Skip("TODO: create a test before merge")
+	testConfig := beforeEach(t)
+
+	type testSetType struct {
+		Description    string
+		ClassHash      *felt.Felt
+		ExpectedResult *CasmCompiledContractClass
+		ExpectedError  error
+	}
+	testSet := map[string][]testSetType{
+		"mock":   {},
+		"devnet": {},
+		"testnet": {
+			{
+				Description:    "normal call, only required field class_hash",
+				ClassHash:      utils.TestHexToFelt(t, "0x00d764f235da1c654c4ca14c47bfc2a54ccd4c0c56b3f4570cd241bd638db448"),
+				ExpectedResult: utils.UnmarshallFileToType[CasmCompiledContractClass](t, "./tests/compiledCasm.json", true),
+			},
+		},
+		"mainnet": {},
+	}[testEnv]
+
+	for _, test := range testSet {
+		require := require.New(t)
+		result, err := testConfig.provider.CompiledCasm(context.Background(), test.ClassHash)
+		require.NoError(err)
+		require.NotNil(result, "should return a nonce")
+		require.Equal(test.ExpectedResult, result)
+	}
 }
