9e3d6fc0a0be1c7e5ff3afe61f07e4bbc1a1b580
thiagodeev <thiagodeev@gmail.com>
2025-02-13 22:18:09 -0300

    Add ErrClassHashNotFound test case to TestCompiledCasm

--- rpc/executables_test.go
+++ rpc/executables_test.go
@@ -26,32 +26,47 @@ func TestCompiledCasm(t *testing.T) {
 		"devnet": {},
 		"testnet": {
 			{
-				Description:        "normal call, only required field class_hash",
+				Description:        "normal call, with field class_hash",
 				ClassHash:          utils.TestHexToFelt(t, "0x00d764f235da1c654c4ca14c47bfc2a54ccd4c0c56b3f4570cd241bd638db448"),
 				ExpectedResultPath: "./tests/compiledCasm.json",
 			},
+			{
+				Description:   "error call, inexistent class_hash",
+				ClassHash:     utils.TestHexToFelt(t, "0xdedededededede"),
+				ExpectedError: ErrClassHashNotFound,
+			},
+			// TODO: add test for compilation error. Need to find the invalid class hash.
 		},
 		"mainnet": {},
 	}[testEnv]
 
 	for _, test := range testSet {
-		// getting the expected result from the file
-		rawFile, err := os.ReadFile(test.ExpectedResultPath)
-		require.NoError(t, err)
-		var rpcResponse struct {
-			Result json.RawMessage `json:"result"`
+		var expectedResult *CasmCompiledContractClass
+		var rawFile []byte
+		var err error
+
+		if test.ExpectedResultPath != "" {
+			// getting the expected result from the file
+			rawFile, err = os.ReadFile(test.ExpectedResultPath)
+			require.NoError(t, err)
+			var rpcResponse struct {
+				Result json.RawMessage `json:"result"`
+			}
+			err = json.Unmarshal(rawFile, &rpcResponse)
+			require.NoError(t, err)
+			rawFile = rpcResponse.Result
+			err = json.Unmarshal(rawFile, &expectedResult)
+			require.NoError(t, err)
 		}
-		err = json.Unmarshal(rawFile, &rpcResponse)
-		require.NoError(t, err)
-		rawFile = rpcResponse.Result
-		var expectedResult CasmCompiledContractClass
-		err = json.Unmarshal(rawFile, &expectedResult)
-		require.NoError(t, err)
 
 		// getting the result from the provider and asserting equality
 		result, err := testConfig.provider.CompiledCasm(context.Background(), test.ClassHash)
-		assert.NoError(t, err)
-		assert.Equal(t, &expectedResult, result)
+		assert.Equal(t, err, test.ExpectedError)
+		assert.Equal(t, expectedResult, result)
+
+		if test.ExpectedError != nil {
+			continue
+		}
 
 		// asserting equality of the json results
 		jsonResult, err := json.Marshal(result)
