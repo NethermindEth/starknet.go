a69c10b3e6056508d27f53d799574de5a0ef679b
thiagodeev <thiagodeev@gmail.com>
2025-03-07 00:22:46 -0300

    Finish descriptiveInvoke func

--- examples/invoke/simpleInvoke.go
+++ examples/invoke/simpleInvoke.go
@@ -3,67 +3,16 @@ package main
 import (
 	"context"
 	"fmt"
-	"math/big"
 	"time"
 
 	"github.com/NethermindEth/juno/core/felt"
 	"github.com/NethermindEth/starknet.go/account"
 	"github.com/NethermindEth/starknet.go/rpc"
-	"github.com/NethermindEth/starknet.go/utils"
 
 	setup "github.com/NethermindEth/starknet.go/examples/internal"
 )
 
-// NOTE : Please add in your keys only for testing purposes, in case of a leak you would potentially lose your funds.
-var (
-	someContract   string = "0x0669e24364ce0ae7ec2864fb03eedbe60cfbc9d1c74438d10fa4b86552907d54" //Replace it with the contract that you want to invoke. In this case, an ERC20
-	contractMethod string = "mint"                                                               //Replace it with the function name that you want to invoke
-)
-
-func simpleInvoke() {
-	// Load variables from '.env' file
-	rpcProviderUrl := setup.GetRpcProviderUrl()
-	accountAddress := setup.GetAccountAddress()
-	accountCairoVersion := setup.GetAccountCairoVersion()
-	privateKey := setup.GetPrivateKey()
-	publicKey := setup.GetPublicKey()
-
-	// Initialize connection to RPC provider
-	client, err := rpc.NewProvider(rpcProviderUrl)
-	if err != nil {
-		panic(fmt.Sprintf("Error dialing the RPC provider: %s", err))
-	}
-
-	// Initialize the account memkeyStore (set public and private keys)
-	ks := account.NewMemKeystore()
-	privKeyBI, ok := new(big.Int).SetString(privateKey, 0)
-	if !ok {
-		panic("Fail to convert privKey to bitInt")
-	}
-	ks.Put(publicKey, privKeyBI)
-
-	// Here we are converting the account address to felt
-	accountAddressInFelt, err := utils.HexToFelt(accountAddress)
-	if err != nil {
-		fmt.Println("Failed to transform the account address, did you give the hex address?")
-		panic(err)
-	}
-	// Initialize the account
-	accnt, err := account.NewAccount(client, accountAddressInFelt, publicKey, ks, accountCairoVersion)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Established connection with the client")
-
-	// Converting the contractAddress from hex to felt
-	contractAddress, err := utils.HexToFelt(someContract)
-	if err != nil {
-		panic(err)
-	}
-
-	amount, _ := utils.HexToFelt("0xffffffff")
-
+func simpleInvoke(accnt *account.Account, contractAddress *felt.Felt, contractMethod string, amount *felt.Felt) {
 	// Building the functionCall struct, where :
 	FnCall := rpc.InvokeFunctionCall{
 		ContractAddress: contractAddress,                  //contractAddress is the contract that we want to call
@@ -84,7 +33,7 @@ func simpleInvoke() {
 	time.Sleep(time.Second * 3) // Waiting 3 seconds
 
 	//Getting the transaction status
-	txStatus, err := client.GetTransactionStatus(context.Background(), resp.TransactionHash)
+	txStatus, err := accnt.Provider().GetTransactionStatus(context.Background(), resp.TransactionHash)
 	if err != nil {
 		setup.PanicRPC(err)
 	}
