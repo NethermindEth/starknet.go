a8227aa277ff6c36cfb278ca9d669f02c0ab1d17
Thiago Ribeiro <62709592+thiagodeev@users.noreply.github.com>
2025-04-02 01:15:43 -0300

    Revert SNIP-12 code to be compatible with starknet.js (#667)
    * Revert code to be compatible with starknet.js
    
    * Add new test cases for new starknet.js updates
    
    * Refactor typedData tests to use TypedDataName for clarity
    
    * Add remaining test cases

--- typedData/typedData_test.go
+++ typedData/typedData_test.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -30,6 +31,7 @@ func TestMain(m *testing.M) {
 		"session_MerkleTree",
 		"v1Nested",
 		"allInOne",
+		"example_enumNested",
 	}
 
 	for _, fileName := range fileNames {
@@ -82,63 +84,71 @@ func BMockTypedData(b *testing.B) (ttd TypedData) {
 // - None
 func TestGetMessageHash(t *testing.T) {
 	type testSetType struct {
-		TypedData           TypedData
+		TypedDataName       string
 		Address             string
 		ExpectedMessageHash string
 	}
 	testSet := []testSetType{
 		{
-			TypedData:           typedDataExamples["baseExample"],
+			TypedDataName:       "baseExample",
 			Address:             "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
 			ExpectedMessageHash: "0x6fcff244f63e38b9d88b9e3378d44757710d1b244282b435cb472053c8d78d0",
 		},
 		{
-			TypedData:           typedDataExamples["example_array"],
+			TypedDataName:       "example_array",
 			Address:             "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
 			ExpectedMessageHash: "0x88edea26d6177a8bc545b2e73c960ab7ddd67b46237b386b514e50315ce0f4",
 		},
 		{
-			TypedData:           typedDataExamples["example_baseTypes"],
+			TypedDataName:       "example_baseTypes",
 			Address:             "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
 			ExpectedMessageHash: "0xdb7829db8909c0c5496f5952bcfc4fc894341ce01842537fc4f448743480b6",
 		},
 		{
-			TypedData:           typedDataExamples["example_presetTypes"],
+			TypedDataName:       "example_presetTypes",
 			Address:             "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
 			ExpectedMessageHash: "0x185b339d5c566a883561a88fb36da301051e2c0225deb325c91bb7aa2f3473a",
 		},
 		{
-			TypedData:           typedDataExamples["session_MerkleTree"],
+			TypedDataName:       "session_MerkleTree",
 			Address:             "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
 			ExpectedMessageHash: "0x751fb7d98545f7649d0d0eadc80d770fcd88d8cfaa55590b284f4e1b701ef0a",
 		},
 		{
-			TypedData:           typedDataExamples["mail_StructArray"],
+			TypedDataName:       "mail_StructArray",
 			Address:             "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
 			ExpectedMessageHash: "0x5914ed2764eca2e6a41eb037feefd3d2e33d9af6225a9e7fe31ac943ff712c",
 		},
 		{
-			TypedData:           typedDataExamples["v1Nested"],
+			TypedDataName:       "v1Nested",
 			Address:             "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
 			ExpectedMessageHash: "0x69b57cf0cd7c151c51f9616cc58a1f0a877fec28c8c15ff7537cf777c54a30d",
 		},
 		{
-			TypedData:           typedDataExamples["example_enum"],
+			TypedDataName:       "example_enum",
 			Address:             "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
-			ExpectedMessageHash: "0x416b85b18063b1b3420ab709e9d5e35cb716691d397c5841ce7c5198ee30bf",
+			ExpectedMessageHash: "0x6e61abaf480b1370bbf231f54e298c5f4872f40a6d2dd409ff30accee5bbd1e",
 		},
 		{
-			TypedData:           typedDataExamples["allInOne"],
+			TypedDataName:       "allInOne",
 			Address:             "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
-			ExpectedMessageHash: "0x300dc63cc85a15529bba5ed482009be716645fa9f2c64bd8716cf6f34767651",
+			ExpectedMessageHash: "0x8fa4e453de78c2762493760efd449a38eb46f85b2e02b116b77b3daa9075c8",
+		},
+		{
+			TypedDataName:       "example_enumNested",
+			Address:             "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
+			ExpectedMessageHash: "0x691fc54567306a8ea5431130f1b98299e74a748ac391540a86736f20ef5f2b7",
 		},
 	}
 
 	for _, test := range testSet {
-		hash, err := test.TypedData.GetMessageHash(test.Address)
-		require.NoError(t, err)
+		t.Run(test.TypedDataName, func(t *testing.T) {
+			td := typedDataExamples[test.TypedDataName]
+			hash, err := td.GetMessageHash(test.Address)
+			require.NoError(t, err)
 
-		require.Equal(t, test.ExpectedMessageHash, hash.String())
+			assert.Equal(t, test.ExpectedMessageHash, hash.String())
+		})
 	}
 }
 
@@ -177,42 +187,50 @@ func BenchmarkGetMessageHash(b *testing.B) {
 //	none
 func TestGetTypeHash(t *testing.T) {
 	type testSetType struct {
-		TypedData    TypedData
-		TypeName     string
-		ExpectedHash string
+		TypedDataName string
+		TypeName      string
+		ExpectedHash  string
 	}
 	testSet := []testSetType{
 		{
-			TypedData:    typedDataExamples["baseExample"],
-			TypeName:     "StarkNetDomain",
-			ExpectedHash: "0x1bfc207425a47a5dfa1a50a4f5241203f50624ca5fdf5e18755765416b8e288",
+			TypedDataName: "baseExample",
+			TypeName:      "StarkNetDomain",
+			ExpectedHash:  "0x1bfc207425a47a5dfa1a50a4f5241203f50624ca5fdf5e18755765416b8e288",
 		},
 		{
-			TypedData:    typedDataExamples["baseExample"],
-			TypeName:     "Mail",
-			ExpectedHash: "0x13d89452df9512bf750f539ba3001b945576243288137ddb6c788457d4b2f79",
+			TypedDataName: "baseExample",
+			TypeName:      "Mail",
+			ExpectedHash:  "0x13d89452df9512bf750f539ba3001b945576243288137ddb6c788457d4b2f79",
 		},
 		{
-			TypedData:    typedDataExamples["example_baseTypes"],
-			TypeName:     "Example",
-			ExpectedHash: "0x1f94cd0be8b4097a41486170fdf09a4cd23aefbc74bb2344718562994c2c111",
+			TypedDataName: "example_baseTypes",
+			TypeName:      "Example",
+			ExpectedHash:  "0x1f94cd0be8b4097a41486170fdf09a4cd23aefbc74bb2344718562994c2c111",
 		},
 		{
-			TypedData:    typedDataExamples["example_presetTypes"],
-			TypeName:     "Example",
-			ExpectedHash: "0x1a25a8bb84b761090b1fadaebe762c4b679b0d8883d2bedda695ea340839a55",
+			TypedDataName: "example_presetTypes",
+			TypeName:      "Example",
+			ExpectedHash:  "0x1a25a8bb84b761090b1fadaebe762c4b679b0d8883d2bedda695ea340839a55",
 		},
 		{
-			TypedData:    typedDataExamples["session_MerkleTree"],
-			TypeName:     "Session",
-			ExpectedHash: "0x1aa0e1c56b45cf06a54534fa1707c54e520b842feb21d03b7deddb6f1e340c",
+			TypedDataName: "session_MerkleTree",
+			TypeName:      "Session",
+			ExpectedHash:  "0x1aa0e1c56b45cf06a54534fa1707c54e520b842feb21d03b7deddb6f1e340c",
+		},
+		{
+			TypedDataName: "example_enumNested",
+			TypeName:      "Example",
+			ExpectedHash:  "0x2143bb787fabace39d62e9acf8b6e97d9a369000516c3e6ffd963dc1370fc1a",
 		},
 	}
 	for _, test := range testSet {
-		hash, err := test.TypedData.GetTypeHash(test.TypeName)
-		require.NoError(t, err)
+		t.Run(test.TypedDataName, func(t *testing.T) {
+			td := typedDataExamples[test.TypedDataName]
+			hash, err := td.GetTypeHash(test.TypeName)
+			require.NoError(t, err)
 
-		require.Equal(t, test.ExpectedHash, hash.String())
+			assert.Equal(t, test.ExpectedHash, hash.String())
+		})
 	}
 }
 
@@ -230,59 +248,67 @@ func TestGetTypeHash(t *testing.T) {
 //	none
 func TestEncodeType(t *testing.T) {
 	type testSetType struct {
-		TypedData      TypedData
+		TypedDataName  string
 		TypeName       string
 		ExpectedEncode string
 	}
 	testSet := []testSetType{
 		{
-			TypedData:      typedDataExamples["baseExample"],
+			TypedDataName:  "baseExample",
 			TypeName:       "StarkNetDomain",
 			ExpectedEncode: "StarkNetDomain(name:felt,version:felt,chainId:felt)",
 		},
 		{
-			TypedData:      typedDataExamples["baseExample"],
+			TypedDataName:  "baseExample",
 			TypeName:       "Mail",
 			ExpectedEncode: "Mail(from:Person,to:Person,contents:felt)Person(name:felt,wallet:felt)",
 		},
 		{
-			TypedData:      typedDataExamples["example_array"],
+			TypedDataName:  "example_array",
 			TypeName:       "StarknetDomain",
 			ExpectedEncode: `"StarknetDomain"("name":"shortstring","version":"shortstring","chainId":"shortstring","revision":"shortstring")`,
 		},
 		{
-			TypedData:      typedDataExamples["example_baseTypes"],
+			TypedDataName:  "example_baseTypes",
 			TypeName:       "Example",
 			ExpectedEncode: `"Example"("n0":"felt","n1":"bool","n2":"string","n3":"selector","n4":"u128","n5":"i128","n6":"ContractAddress","n7":"ClassHash","n8":"timestamp","n9":"shortstring")`,
 		},
 		{
-			TypedData:      typedDataExamples["example_presetTypes"],
+			TypedDataName:  "example_presetTypes",
 			TypeName:       "Example",
 			ExpectedEncode: `"Example"("n0":"TokenAmount","n1":"NftId")"NftId"("collection_address":"ContractAddress","token_id":"u256")"TokenAmount"("token_address":"ContractAddress","amount":"u256")"u256"("low":"u128","high":"u128")`,
 		},
 		{
-			TypedData:      typedDataExamples["session_MerkleTree"],
+			TypedDataName:  "session_MerkleTree",
 			TypeName:       "Session",
 			ExpectedEncode: `Session(key:felt,expires:felt,root:merkletree)`,
 		},
 		{
-			TypedData:      typedDataExamples["mail_StructArray"],
+			TypedDataName:  "mail_StructArray",
 			TypeName:       "Mail",
 			ExpectedEncode: `Mail(from:Person,to:Person,posts_len:felt,posts:Post*)Person(name:felt,wallet:felt)Post(title:felt,content:felt)`,
 		},
 		{
-			TypedData:      typedDataExamples["v1Nested"],
+			TypedDataName:  "v1Nested",
 			TypeName:       "TransferERC721",
 			ExpectedEncode: `"TransferERC721"("MessageId":"felt","From":"Account1","To":"Account1","Nft_to_transfer":"Nft","Comment1":"string","Comment2":"string","Comment3":"string")"Account1"("Name":"string","Address":"felt")"Nft"("Collection":"string","Address":"felt","Nft_id":"felt","Negotiated_for":"Transaction")"Transaction"("Qty":"string","Unit":"string","Token_address":"felt","Amount":"felt")`,
 		},
 		{
-			TypedData:      typedDataExamples["example_enum"],
+			TypedDataName:  "example_enum",
 			TypeName:       "Example",
 			ExpectedEncode: `"Example"("someEnum1":"EnumA","someEnum2":"EnumB")"EnumA"("Variant 1":(),"Variant 2":("u128","u128*"),"Variant 3":("u128"))"EnumB"("Variant 1":(),"Variant 2":("u128"))`,
 		},
+		{
+			TypedDataName:  "example_enumNested",
+			TypeName:       "Example",
+			ExpectedEncode: `"Example"("someEnum":"EnumA")"EnumA"("Variant 1":(),"Variant 2":("u128","StructA"))"EnumB"("Variant A":(),"Variant B":("StructB*"))"StructA"("nestedEnum":"EnumB")"StructB"("flag":"bool")`,
+		},
 	}
 	for _, test := range testSet {
-		require.Equal(t, test.ExpectedEncode, test.TypedData.Types[test.TypeName].EncoddingString)
+		t.Run(test.TypedDataName, func(t *testing.T) {
+			td := typedDataExamples[test.TypedDataName]
+			assert.Equal(t, test.ExpectedEncode, td.Types[test.TypeName].EncoddingString)
+		})
 	}
 }
 
@@ -300,47 +326,48 @@ func TestEncodeType(t *testing.T) {
 //	none
 func TestGetStructHash(t *testing.T) {
 	type testSetType struct {
-		TypedData    TypedData
-		TypeName     string
-		Context      []string
-		ExpectedHash string
+		TypedDataName string
+		TypeName      string
+		Context       []string
+		ExpectedHash  string
 	}
 	testSet := []testSetType{
 		{
-			TypedData:    typedDataExamples["baseExample"],
-			TypeName:     "StarkNetDomain",
-			ExpectedHash: "0x54833b121883a3e3aebff48ec08a962f5742e5f7b973469c1f8f4f55d470b07",
+			TypedDataName: "baseExample",
+			TypeName:      "StarkNetDomain",
+			ExpectedHash:  "0x54833b121883a3e3aebff48ec08a962f5742e5f7b973469c1f8f4f55d470b07",
 		},
 		{
-			TypedData:    typedDataExamples["example_baseTypes"],
-			TypeName:     "Example",
-			ExpectedHash: "0x75db031c1f5bf980cc48f46943b236cb85a95c8f3b3c8203572453075d3d39",
+			TypedDataName: "example_baseTypes",
+			TypeName:      "Example",
+			ExpectedHash:  "0x75db031c1f5bf980cc48f46943b236cb85a95c8f3b3c8203572453075d3d39",
 		},
 		{
-			TypedData:    typedDataExamples["example_presetTypes"],
-			TypeName:     "Example",
-			ExpectedHash: "0x74fba3f77f8a6111a9315bac313bf75ecfa46d1234e0fda60312fb6a6517667",
+			TypedDataName: "example_presetTypes",
+			TypeName:      "Example",
+			ExpectedHash:  "0x74fba3f77f8a6111a9315bac313bf75ecfa46d1234e0fda60312fb6a6517667",
 		},
 		{
-			TypedData:    typedDataExamples["session_MerkleTree"],
-			TypeName:     "Session",
-			ExpectedHash: "0x73602062421caf6ad2e942253debfad4584bff58930981364dcd378021defe8",
+			TypedDataName: "session_MerkleTree",
+			TypeName:      "Session",
+			ExpectedHash:  "0x73602062421caf6ad2e942253debfad4584bff58930981364dcd378021defe8",
 		},
 		{
-			TypedData:    typedDataExamples["v1Nested"],
-			TypeName:     "TransferERC721",
-			ExpectedHash: "0x11b5fb80dd88c3d8b6239b065def4ac9a79e6995b117ed5940a3a0734324b79",
+			TypedDataName: "v1Nested",
+			TypeName:      "TransferERC721",
+			ExpectedHash:  "0x11b5fb80dd88c3d8b6239b065def4ac9a79e6995b117ed5940a3a0734324b79",
 		},
 		{
-			TypedData:    typedDataExamples["example_enum"],
-			TypeName:     "Example",
-			ExpectedHash: "0x1551dc992033e2256a2f7ec849495d90f9759ebb535e3006d16e2b9e3b57b4c",
+			TypedDataName: "example_enum",
+			TypeName:      "Example",
+			ExpectedHash:  "0x1e1bb5d477e92cbf562b3b766c5c1e5f8590f2df868d4c8249c0db8416f8c37",
 		},
 	}
 	for _, test := range testSet {
-		hash, err := test.TypedData.GetStructHash(test.TypeName, test.Context...)
+		td := typedDataExamples[test.TypedDataName]
+		hash, err := td.GetStructHash(test.TypeName, test.Context...)
 		require.NoError(t, err)
 
-		require.Equal(t, test.ExpectedHash, hash.String())
+		assert.Equal(t, test.ExpectedHash, hash.String())
 	}
 }
