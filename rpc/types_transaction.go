085cf44d55c729c5353c1ed836830865413a417c
thiagodeev <thiagodeev@gmail.com>
2025-04-16 11:03:23 -0300

    Remove unused TXN type

--- rpc/types_transaction.go
+++ rpc/types_transaction.go
@@ -11,30 +11,6 @@ import (
 	internalUtils "github.com/NethermindEth/starknet.go/internal/utils"
 )
 
-// https://github.com/starkware-libs/starknet-specs/blob/a789ccc3432c57777beceaa53a34a7ae2f25fda0/api/starknet_api_openrpc.json#L1252
-type TXN struct {
-	Hash                  *felt.Felt            `json:"transaction_hash,omitempty"`
-	Type                  TransactionType       `json:"type"`
-	Version               *felt.Felt            `json:"version,omitempty"`
-	Nonce                 *felt.Felt            `json:"nonce,omitempty"`
-	MaxFee                *felt.Felt            `json:"max_fee,omitempty"`
-	ContractAddress       *felt.Felt            `json:"contract_address,omitempty"`
-	ContractAddressSalt   *felt.Felt            `json:"contract_address_salt,omitempty"`
-	ClassHash             *felt.Felt            `json:"class_hash,omitempty"`
-	ConstructorCalldata   []*felt.Felt          `json:"constructor_calldata,omitempty"`
-	SenderAddress         *felt.Felt            `json:"sender_address,omitempty"`
-	Signature             *[]*felt.Felt         `json:"signature,omitempty"`
-	Calldata              *[]*felt.Felt         `json:"calldata,omitempty"`
-	EntryPointSelector    *felt.Felt            `json:"entry_point_selector,omitempty"`
-	CompiledClassHash     *felt.Felt            `json:"compiled_class_hash,omitempty"`
-	ResourceBounds        ResourceBoundsMapping `json:"resource_bounds"`
-	Tip                   U64                   `json:"tip"`
-	PayMasterData         []*felt.Felt          `json:"paymaster_data"`
-	AccountDeploymentData []*felt.Felt          `json:"account_deployment_data"`
-	NonceDataMode         DataAvailabilityMode  `json:"nonce_data_availability_mode"`
-	FeeMode               DataAvailabilityMode  `json:"fee_data_availability_mode"`
-}
-
 type InvokeTxnV0 struct {
 	Type      TransactionType    `json:"type"`
 	MaxFee    *felt.Felt         `json:"max_fee"`
