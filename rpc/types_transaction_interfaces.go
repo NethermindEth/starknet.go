08745db1781631c7be86b8829ffb952e44483b4d
thiagodeev <thiagodeev@gmail.com>
2025-04-16 12:36:48 -0300

    Add GetVersion() method for the Transaction interface

--- rpc/types_transaction_interfaces.go
+++ rpc/types_transaction_interfaces.go
@@ -2,6 +2,7 @@ package rpc
 
 type Transaction interface {
 	GetType() TransactionType
+	GetVersion() TransactionVersion
 }
 
 var _ Transaction = InvokeTxnV0{}
@@ -15,48 +16,80 @@ var _ Transaction = DeployAccountTxn{}
 var _ Transaction = DeployAccountTxnV3{}
 var _ Transaction = L1HandlerTxn{}
 
+// Invoke transactions
 func (tx InvokeTxnV0) GetType() TransactionType {
 	return tx.Type
 }
-
+func (tx InvokeTxnV0) GetVersion() TransactionVersion {
+	return tx.Version
+}
 func (tx InvokeTxnV1) GetType() TransactionType {
 	return tx.Type
 }
-
+func (tx InvokeTxnV1) GetVersion() TransactionVersion {
+	return tx.Version
+}
 func (tx InvokeTxnV3) GetType() TransactionType {
 	return tx.Type
 }
+func (tx InvokeTxnV3) GetVersion() TransactionVersion {
+	return tx.Version
+}
 
+// Declare transactions
 func (tx DeclareTxnV0) GetType() TransactionType {
 	return tx.Type
 }
-
+func (tx DeclareTxnV0) GetVersion() TransactionVersion {
+	return tx.Version
+}
 func (tx DeclareTxnV1) GetType() TransactionType {
 	return tx.Type
 }
-
+func (tx DeclareTxnV1) GetVersion() TransactionVersion {
+	return tx.Version
+}
 func (tx DeclareTxnV2) GetType() TransactionType {
 	return tx.Type
 }
-
+func (tx DeclareTxnV2) GetVersion() TransactionVersion {
+	return tx.Version
+}
 func (tx DeclareTxnV3) GetType() TransactionType {
 	return tx.Type
 }
+func (tx DeclareTxnV3) GetVersion() TransactionVersion {
+	return tx.Version
+}
 
+// Deploy transaction
 func (tx DeployTxn) GetType() TransactionType {
 	return tx.Type
 }
+func (tx DeployTxn) GetVersion() TransactionVersion {
+	return tx.Version
+}
 
+// DeployAccount transactions
 func (tx DeployAccountTxn) GetType() TransactionType {
 	return tx.Type
 }
-
+func (tx DeployAccountTxn) GetVersion() TransactionVersion {
+	return tx.Version
+}
 func (tx DeployAccountTxnV3) GetType() TransactionType {
 	return tx.Type
 }
+func (tx DeployAccountTxnV3) GetVersion() TransactionVersion {
+	return tx.Version
+}
 
+// L1Handler transaction
 func (tx L1HandlerTxn) GetType() TransactionType {
 	return tx.Type
+}
+func (tx L1HandlerTxn) GetVersion() TransactionVersion {
+	return tx.Version
 }
 
 // Note: these allow all types to pass, but are to help users of starknet.go
