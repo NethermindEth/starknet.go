e75ff7d615a7162e3a7f776c350ca6b4e2933e31
Musa AbdulKareem <abdulkareemusa01@gmail.com>
2025-04-23 09:49:29 +0100

    docs: documentation is added for all the modules and tailwindcss is configure

--- www/sidebar.ts
+++ www/sidebar.ts
@@ -1,28 +1,88 @@
 import type { Sidebar } from 'vocs'
 
 export const sidebar: Sidebar = {
-    '/docs/': [
+    '/': [
         {
             text: 'Introduction',
             items: [
-                { text: 'Why Starknet.go?', link: '/docs/introduction/why-starknet-go' },
-                { text: 'Installation', link: '/docs/introduction/installation' },
-                { text: 'Getting Started', link: '/docs/introduction/getting-started' },
-                { text: 'Contributing', link: '/docs/introduction/contributing' },
-                { text: 'Platform Compatibility', link: '/docs/introduction/platform-compatibility' },
-                { text: 'FAQ', link: '/docs/introduction/faq' },
+                { text: 'Why Starknet.go?', link: '/introduction/why-starknet-go' },
+                { text: 'Installation', link: '/introduction/installation' },
+                { text: 'Getting Started', link: '/introduction/getting-started' },
+                { text: 'Contributing', link: '/introduction/contributing' },
             ],
         },
         {
             text: 'Guides',
             items: [
-                { text: 'How to use Starknet.go', link: '/docs/guides/how-to-use-starknet-go' },
+                { text: 'How to use Starknet.go', link: '/introduction/guides/how-to-use-starknet-go' },
             ],
         },
-        {
-            text: 'Examples',
+        { 
+            text: 'Account',
+            collapsed: true,
             items: [
-                { text: 'Example', link: '/docs/example/example' },
+                { text: 'Overview', link: '/account/' },
+                { text: 'Account Management', link: '/account/account-management' },
+                { text: 'Transaction Handling', link: '/account/transaction-handling' },
+                { text: 'Signature Verification', link: '/account/signature-verification' },
+                { text: 'Account Utilities', link: '/account/account-utilities' },
+            ]
+        },
+        {
+            text: 'Client',
+            collapsed: true,
+            items: [
+                { text: 'Overview', link: '/client/' },
+                { text: 'Client Configuration', link: '/client/configuration' },
+                { text: 'Client Methods', link: '/client/methods' },
+                { text: 'Client Examples', link: '/client/examples' },
+            ]
+        },
+        {
+            text: 'RPC',
+            collapsed: true,
+            items: [
+                { text: 'Overview', link: '/rpc/' },
+                { text: 'RPC Methods', link: '/rpc/methods' },
+                { text: 'RPC Examples', link: '/rpc/examples' },
+            ]
+        },
+        {
+            text: 'Curves',
+            collapsed: true,
+            items: [
+                { text: 'Overview', link: '/curves/' },
+                { text: 'Curve Types', link: '/curves/types' },
+                { text: 'Curve Operations', link: '/curves/operations' },
+            ]
+        },
+        {
+            text: 'Utilities',
+            collapsed: true,
+            items: [
+                { text: 'Overview', link: '/utilities/' },
+                { text: 'Common Utilities', link: '/utilities/common' },
+                { text: 'Type Utilities', link: '/utilities/types' },
+            ]
+        },
+        { 
+            text: 'Developer Tools',
+            collapsed: true,
+            items: [
+                { text: 'Overview', link: '/devnet/' },
+                { text: 'Devnet Setup', link: '/devnet/setup' },
+                { text: 'Devnet Usage', link: '/devnet/usage' },
+            ]
+        },
+        {
+            text: 'ABI',
+            collapsed: true,
+            items: [
+                { text: 'Overview', link: '/abi/' },
+                { text: 'ABI Types', link: '/abi/types' },
+                { text: 'ABI Encoding', link: '/abi/encoding' },
+                { text: 'ABI Decoding', link: '/abi/decoding' },
+
             ],
         },
     ],
--- www/vocs.config.ts
+++ www/vocs.config.ts
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vocs'
 import { sidebar } from './sidebar'
-
 export default defineConfig({
   title: 'starknet.go',
+  titleTemplate: '%s · starknet.go',
+  editLink: {
+    pattern: 'https://github.com/NethermindEth/starknet.go/edit/main/www/docs/pages/:path',
+    text: 'Suggest changes to this page',
+  },
   sidebar,
+  iconUrl: { light: '/logo.svg', dark: '/logo.svg' },
+  logoUrl: { light: '/starknetgo_vertical_light.png', dark: '/starknetgo_vertical_dark.png' },
+  socials: [
+    {
+      icon: 'github',
+      link: 'https://github.com/NethermindEth/starknet.go',
+    },
+    {
+      icon: 'x',
+      link: 'https://x.com/NethermindStark',
+    },
+    {
+      icon: 'telegram',
+      link: 'https://t.me/StarknetGo',
+    },
+  ],
+  theme: {
+    accentColor: {
+      light: '#ff9318',
+      dark: '#ffc517',
+    }
+  },
+  topNav: [
+    { text: 'Docs', link: '/introduction/why-starknet-go', match: '/docs' }
+  ],
+  
 })
