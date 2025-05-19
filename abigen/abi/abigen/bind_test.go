798979c5e2b39041584f1eec04b66e49cbdc7ac0
thiagodeev <thiagodeev@gmail.com>
2025-05-19 10:42:39 -0300

    fix: update module path from ethereum to NethermindEth/starknet.go/abigen in bind test

--- abigen/abi/abigen/bind_test.go
+++ abigen/abi/abigen/bind_test.go
@@ -2144,7 +2144,7 @@ func TestBindings(t *testing.T) {
 		t.Fatalf("failed to convert binding test to modules: %v\n%s", err, out)
 	}
 	pwd, _ := os.Getwd()
-	replacer := exec.Command(gocmd, "mod", "edit", "-x", "-require", "github.com/ethereum/go-ethereum@v0.0.0", "-replace", "github.com/ethereum/go-ethereum="+filepath.Join(pwd, "..", "..", "..")) // Repo root
+	replacer := exec.Command(gocmd, "mod", "edit", "-x", "-require", "github.com/NethermindEth/starknet.go/abigen@v0.0.0", "-replace", "github.com/NethermindEth/starknet.go/abigen="+filepath.Join(pwd, "..", "..", "..")) // Repo root
 	replacer.Dir = pkg
 	if out, err := replacer.CombinedOutput(); err != nil {
 		t.Fatalf("failed to replace binding test dependency to current source tree: %v\n%s", err, out)
