e7dbea8cf996ba9456700ef4dc623818a95e9535
thiagodeev <thiagodeev@gmail.com>
2025-09-05 14:55:32 -0300

    test: add integration test for BuildTransaction and improve parallel execution in existing tests

--- paymaster/build_txn_test.go
+++ paymaster/build_txn_test.go
@@ -4,11 +4,15 @@ import (
 	"encoding/json"
 	"testing"
 
+	"github.com/NethermindEth/juno/core/felt"
+	"github.com/NethermindEth/starknet.go/account"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 // Test the UserTxnType type
 func TestUserTxnType(t *testing.T) {
+	t.Parallel()
 	type testCase struct {
 		Input         string
 		Expected      UserTxnType
@@ -39,6 +43,7 @@ func TestUserTxnType(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.Input, func(t *testing.T) {
+			t.Parallel()
 			CompareEnumsHelper(t, test.Input, test.Expected, test.ErrorExpected)
 		})
 	}
@@ -46,6 +51,7 @@ func TestUserTxnType(t *testing.T) {
 
 // Test the FeeModeType type
 func TestFeeModeType(t *testing.T) {
+	t.Parallel()
 	type testCase struct {
 		Input         string
 		Expected      FeeModeType
@@ -76,6 +82,7 @@ func TestFeeModeType(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.Input, func(t *testing.T) {
+			t.Parallel()
 			CompareEnumsHelper(t, test.Input, test.Expected, test.ErrorExpected)
 		})
 	}
@@ -97,4 +104,46 @@ func CompareEnumsHelper[T any](t *testing.T, input string, expected T, errorExpe
 		assert.NoError(t, err)
 		assert.Equal(t, input, string(marshalled))
 	}
+}
+
+func TestBuildTransaction(t *testing.T) {
+	t.Parallel()
+	t.Run("integration", func(t *testing.T) {
+		t.Parallel()
+		// pm := SetupPaymaster(t)
+
+		_, pubK, _ := account.GetRandomKeys()
+
+		classHash := new(felt.Felt).SetBytes([]byte("0x61dac032f228abef9c6626f995015233097ae253a7f72d68552db02f2971b8f"))
+		salt := new(felt.Felt).SetBytes([]byte("0xdeadbeef"))
+
+		precomputedAddress := account.PrecomputeAccountAddress(salt, classHash, []*felt.Felt{pubK})
+
+		request := &BuildTransactionRequest{
+			Transaction: &UserTransaction{
+				Type: UserTxnDeploy,
+				Deployment: &AccDeploymentData{
+					Address:             precomputedAddress,
+					ClassHash:           classHash,
+					Salt:                salt,
+					ConstructorCalldata: []*felt.Felt{pubK},
+					Version:             2,
+				},
+			},
+			Parameters: &UserParameters{
+				Version: "0x1",
+				FeeMode: FeeMode{
+					Mode: FeeModeSponsored,
+				},
+			},
+		}
+		raw, err := json.Marshal(request)
+		require.NoError(t, err)
+		t.Logf("Raw: %s", string(raw))
+
+		// tokens, err := pm.BuildTransaction(context.Background(), request)
+		// require.NoError(t, err)
+		// assert.NotNil(t, tokens)
+
+	})
 }
