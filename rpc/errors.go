035b28904e7aec203df47ce85546f408518fa24f
thiagodeev <thiagodeev@gmail.com>
2025-09-09 13:01:51 -0300

    refactor: simplify error handling by creating aliases for RPCError types
    - Introduced type aliases for RPCError and StringErrData to streamline error handling.
    - Updated all instances of rpcerr.RPCError to use the new alias for consistency and clarity.

--- rpc/errors.go
+++ rpc/errors.go
@@ -9,10 +9,13 @@ import (
 	"github.com/NethermindEth/starknet.go/rpcerr"
 )
 
+// aliases to facilitate usage
+
+type RPCError = rpcerr.RPCError
+type StringErrData = rpcerr.StringErrData
+
 //nolint:exhaustruct
 var (
-	_ rpcerr.RPCData = rpcerr.StringErrData("")
-
 	_ rpcerr.RPCData = &CompilationErrData{}
 	_ rpcerr.RPCData = &ContractErrData{}
 	_ rpcerr.RPCData = &TransactionExecErrData{}
@@ -21,158 +24,158 @@ var (
 
 //nolint:exhaustruct
 var (
-	ErrFailedToReceiveTxn = &rpcerr.RPCError{
+	ErrFailedToReceiveTxn = &RPCError{
 		Code:    1,
 		Message: "Failed to write transaction",
 	}
-	ErrNoTraceAvailable = &rpcerr.RPCError{
+	ErrNoTraceAvailable = &RPCError{
 		Code:    10,
 		Message: "No trace available for transaction",
 		Data:    &TraceStatusErrData{},
 	}
-	ErrContractNotFound = &rpcerr.RPCError{
+	ErrContractNotFound = &RPCError{
 		Code:    20,
 		Message: "Contract not found",
 	}
-	ErrEntrypointNotFound = &rpcerr.RPCError{
+	ErrEntrypointNotFound = &RPCError{
 		Code:    21,
 		Message: "Requested entrypoint does not exist in the contract",
 	}
-	ErrBlockNotFound = &rpcerr.RPCError{
+	ErrBlockNotFound = &RPCError{
 		Code:    24,
 		Message: "Block not found",
 	}
-	ErrInvalidTxnHash = &rpcerr.RPCError{
+	ErrInvalidTxnHash = &RPCError{
 		Code:    25,
 		Message: "Invalid transaction hash",
 	}
-	ErrInvalidBlockHash = &rpcerr.RPCError{
+	ErrInvalidBlockHash = &RPCError{
 		Code:    26,
 		Message: "Invalid block hash",
 	}
-	ErrInvalidTxnIndex = &rpcerr.RPCError{
+	ErrInvalidTxnIndex = &RPCError{
 		Code:    27,
 		Message: "Invalid transaction index in a block",
 	}
-	ErrClassHashNotFound = &rpcerr.RPCError{
+	ErrClassHashNotFound = &RPCError{
 		Code:    28,
 		Message: "Class hash not found",
 	}
-	ErrHashNotFound = &rpcerr.RPCError{
+	ErrHashNotFound = &RPCError{
 		Code:    29,
 		Message: "Transaction hash not found",
 	}
-	ErrPageSizeTooBig = &rpcerr.RPCError{
+	ErrPageSizeTooBig = &RPCError{
 		Code:    31,
 		Message: "Requested page size is too big",
 	}
-	ErrNoBlocks = &rpcerr.RPCError{
+	ErrNoBlocks = &RPCError{
 		Code:    32,
 		Message: "There are no blocks",
 	}
-	ErrInvalidContinuationToken = &rpcerr.RPCError{
+	ErrInvalidContinuationToken = &RPCError{
 		Code:    33,
 		Message: "The supplied continuation token is invalid or unknown",
 	}
-	ErrTooManyKeysInFilter = &rpcerr.RPCError{
+	ErrTooManyKeysInFilter = &RPCError{
 		Code:    34,
 		Message: "Too many keys provided in a filter",
 	}
-	ErrContractError = &rpcerr.RPCError{
+	ErrContractError = &RPCError{
 		Code:    40,
 		Message: "Contract error",
 		Data:    &ContractErrData{},
 	}
-	ErrTxnExec = &rpcerr.RPCError{
+	ErrTxnExec = &RPCError{
 		Code:    41,
 		Message: "Transaction execution error",
 		Data:    &TransactionExecErrData{},
 	}
-	ErrStorageProofNotSupported = &rpcerr.RPCError{
+	ErrStorageProofNotSupported = &RPCError{
 		Code:    42,
 		Message: "the node doesn't support storage proofs for blocks that are too far in the past",
 	}
-	ErrInvalidContractClass = &rpcerr.RPCError{
+	ErrInvalidContractClass = &RPCError{
 		Code:    50,
 		Message: "Invalid contract class",
 	}
-	ErrClassAlreadyDeclared = &rpcerr.RPCError{
+	ErrClassAlreadyDeclared = &RPCError{
 		Code:    51,
 		Message: "Class already declared",
 	}
-	ErrInvalidTransactionNonce = &rpcerr.RPCError{
+	ErrInvalidTransactionNonce = &RPCError{
 		Code:    52,
 		Message: "Invalid transaction nonce",
 		Data:    rpcerr.StringErrData(""),
 	}
-	ErrInsufficientResourcesForValidate = &rpcerr.RPCError{
+	ErrInsufficientResourcesForValidate = &RPCError{
 		Code:    53,
 		Message: "The transaction's resources don't cover validation or the minimal transaction fee",
 	}
-	ErrInsufficientAccountBalance = &rpcerr.RPCError{
+	ErrInsufficientAccountBalance = &RPCError{
 		Code:    54,
 		Message: "Account balance is smaller than the transaction's maximal fee (calculated as the sum of each resource's limit x max price)",
 	}
-	ErrValidationFailure = &rpcerr.RPCError{
+	ErrValidationFailure = &RPCError{
 		Code:    55,
 		Message: "Account validation failed",
 		Data:    rpcerr.StringErrData(""),
 	}
-	ErrCompilationFailed = &rpcerr.RPCError{
+	ErrCompilationFailed = &RPCError{
 		Code:    56,
 		Message: "Compilation failed",
 		Data:    rpcerr.StringErrData(""),
 	}
-	ErrContractClassSizeTooLarge = &rpcerr.RPCError{
+	ErrContractClassSizeTooLarge = &RPCError{
 		Code:    57,
 		Message: "Contract class size is too large",
 	}
-	ErrNonAccount = &rpcerr.RPCError{
+	ErrNonAccount = &RPCError{
 		Code:    58,
 		Message: "Sender address is not an account contract",
 	}
-	ErrDuplicateTx = &rpcerr.RPCError{
+	ErrDuplicateTx = &RPCError{
 		Code:    59,
 		Message: "A transaction with the same hash already exists in the mempool",
 	}
-	ErrCompiledClassHashMismatch = &rpcerr.RPCError{
+	ErrCompiledClassHashMismatch = &RPCError{
 		Code:    60,
 		Message: "The compiled class hash did not match the one supplied in the transaction",
 	}
-	ErrUnsupportedTxVersion = &rpcerr.RPCError{
+	ErrUnsupportedTxVersion = &RPCError{
 		Code:    61,
 		Message: "The transaction version is not supported",
 	}
-	ErrUnsupportedContractClassVersion = &rpcerr.RPCError{
+	ErrUnsupportedContractClassVersion = &RPCError{
 		Code:    62,
 		Message: "The contract class version is not supported",
 	}
-	ErrUnexpectedError = &rpcerr.RPCError{
+	ErrUnexpectedError = &RPCError{
 		Code:    63,
 		Message: "An unexpected error occurred",
 		Data:    rpcerr.StringErrData(""),
 	}
-	ErrReplacementTransactionUnderpriced = &rpcerr.RPCError{
+	ErrReplacementTransactionUnderpriced = &RPCError{
 		Code:    64,
 		Message: "Replacement transaction is underpriced",
 	}
-	ErrFeeBelowMinimum = &rpcerr.RPCError{
+	ErrFeeBelowMinimum = &RPCError{
 		Code:    65,
 		Message: "Transaction fee below minimum",
 	}
-	ErrInvalidSubscriptionID = &rpcerr.RPCError{
+	ErrInvalidSubscriptionID = &RPCError{
 		Code:    66,
 		Message: "Invalid subscription id",
 	}
-	ErrTooManyAddressesInFilter = &rpcerr.RPCError{
+	ErrTooManyAddressesInFilter = &RPCError{
 		Code:    67,
 		Message: "Too many addresses in filter sender_address filter",
 	}
-	ErrTooManyBlocksBack = &rpcerr.RPCError{
+	ErrTooManyBlocksBack = &RPCError{
 		Code:    68,
 		Message: "Cannot go back more than 1024 blocks",
 	}
-	ErrCompilationError = &rpcerr.RPCError{
+	ErrCompilationError = &RPCError{
 		Code:    100,
 		Message: "Failed to compile the contract",
 		Data:    &CompilationErrData{},
