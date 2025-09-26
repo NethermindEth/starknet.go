d9d97a11207c9d5ead0f0bffb6f3c9c2449744d6
thiagodeev <thiagodeev@gmail.com>
2025-09-26 12:11:14 -0300

    refactor: separate the IsAvailable method and related tests to separate files

--- paymaster/paymaster.go
+++ paymaster/paymaster.go
@@ -64,24 +64,6 @@ func NewPaymasterClient(url string, options ...client.ClientOption) (*Paymaster,
 	return paymaster, nil
 }
 
-// IsAvailable returns the status of the paymaster service.
-// If the paymaster service is correctly functioning, return true. Else, return false
-//
-// Parameters:
-//   - ctx: The context.Context object for controlling the function call
-//
-// Returns:
-//   - bool: True if the paymaster service is correctly functioning, false otherwise
-//   - error: An error if any
-func (p *Paymaster) IsAvailable(ctx context.Context) (bool, error) {
-	var response bool
-	if err := p.c.CallContextWithSliceArgs(ctx, &response, "paymaster_isAvailable"); err != nil {
-		return false, err
-	}
-
-	return response, nil
-}
-
 // Get a list of the tokens that the paymaster supports, together with their prices in STRK
 //
 // Parameters:
--- paymaster/paymaster_test.go
+++ paymaster/paymaster_test.go
@@ -3,7 +3,6 @@ package paymaster
 import (
 	"context"
 	"encoding/json"
-	"strconv"
 	"testing"
 
 	"github.com/NethermindEth/starknet.go/internal/tests"
@@ -11,36 +10,6 @@ import (
 	"github.com/stretchr/testify/require"
 	"go.uber.org/mock/gomock"
 )
-
-// Test the 'paymaster_isAvailable' method
-func TestIsAvailable(t *testing.T) {
-	t.Parallel()
-	t.Run("integration", func(t *testing.T) {
-		tests.RunTestOn(t, tests.IntegrationEnv)
-		t.Parallel()
-
-		pm, spy := SetupPaymaster(t)
-		available, err := pm.IsAvailable(context.Background())
-		require.NoError(t, err)
-
-		assert.Equal(t, string(spy.LastResponse()), strconv.FormatBool(available))
-		assert.True(t, available)
-	})
-
-	t.Run("mock", func(t *testing.T) {
-		tests.RunTestOn(t, tests.MockEnv)
-		t.Parallel()
-
-		pm := SetupMockPaymaster(t)
-		pm.c.EXPECT().
-			CallContextWithSliceArgs(context.Background(), gomock.AssignableToTypeOf(new(bool)), "paymaster_isAvailable").
-			SetArg(1, true).
-			Return(nil)
-		available, err := pm.IsAvailable(context.Background())
-		assert.NoError(t, err)
-		assert.True(t, available)
-	})
-}
 
 // Test the 'paymaster_getSupportedTokens' method
 func TestGetSupportedTokens(t *testing.T) {
