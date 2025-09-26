f181af6462ae8279b7c16bea57fca2e265e8e426
thiagodeev <thiagodeev@gmail.com>
2025-09-26 12:53:53 -0300

    test: add unit test for TrackingIdToLatestHash method in paymaster
    - The test mocks the paymaster environment and checks the response against expected values.
    - Ensured proper error handling and JSON response validation within the test.

---
+++ paymaster/tracking_id_test.go
@@ -0,0 +1,51 @@
+package paymaster
+
+import (
+	"context"
+	"encoding/json"
+	"testing"
+
+	"github.com/NethermindEth/starknet.go/internal/tests"
+	internalUtils "github.com/NethermindEth/starknet.go/internal/utils"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+)
+
+// Test the 'paymaster_trackingIdToLatestHash' method
+func TestTrackingIdToLatestHash(t *testing.T) {
+	// The AVNU paymaster does not support this method yet, so we can't have integration tests
+	tests.RunTestOn(t, tests.MockEnv)
+	t.Parallel()
+
+	expectedRawResp := `{
+		"transaction_hash": "0xdeadbeef",
+		"status": "active"
+	}`
+
+	var expectedResp TrackingIdResponse
+	err := json.Unmarshal([]byte(expectedRawResp), &expectedResp)
+	require.NoError(t, err)
+
+	trackingId := internalUtils.RANDOM_FELT
+
+	pm := SetupMockPaymaster(t)
+	pm.c.EXPECT().
+		CallContextWithSliceArgs(
+			context.Background(),
+			gomock.AssignableToTypeOf(new(TrackingIdResponse)),
+			"paymaster_trackingIdToLatestHash",
+			trackingId,
+		).
+		SetArg(1, expectedResp).
+		Return(nil)
+
+	response, err := pm.TrackingIdToLatestHash(context.Background(), trackingId)
+	require.NoError(t, err)
+	assert.Equal(t, TxnActive, response.Status)
+	assert.Equal(t, expectedResp.TransactionHash, response.TransactionHash)
+
+	rawResp, err := json.Marshal(response)
+	require.NoError(t, err)
+	assert.JSONEq(t, expectedRawResp, string(rawResp))
+}
