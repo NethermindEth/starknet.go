a005aa4e1df181cac5e4549880ae8dee500bc0a8
thiagodeev <thiagodeev@gmail.com>
2025-09-26 13:08:20 -0300

    refactor: move TokenData type to the get_tokens.go file to better organization

--- paymaster/get_tokens.go
+++ paymaster/get_tokens.go
@@ -1,6 +1,10 @@
 package paymaster
 
-import "context"
+import (
+	"context"
+
+	"github.com/NethermindEth/juno/core/felt"
+)
 
 // Get a list of the tokens that the paymaster supports, together with their prices in STRK
 //
@@ -17,4 +21,14 @@ func (p *Paymaster) GetSupportedTokens(ctx context.Context) ([]TokenData, error)
 	}
 
 	return response, nil
+}
+
+// Object containing data about the token: contract address, number of decimals and current price in STRK
+type TokenData struct {
+	// Token contract address
+	TokenAddress *felt.Felt `json:"token_address"`
+	// The number of decimals of the token
+	Decimals uint8 `json:"decimals"`
+	// Price in STRK (in FRI units)
+	PriceInStrk string `json:"price_in_strk"` // u256 as a hex string
 }
--- paymaster/types_paymaster.go
+++ paymaster/types_paymaster.go
@@ -1,7 +1,6 @@
 package paymaster
 
 import (
-	"github.com/NethermindEth/juno/core/felt"
 	"github.com/NethermindEth/starknet.go/client/rpcerr"
 )
 
@@ -82,14 +81,4 @@ type TxnExecutionErrData struct {
 // ErrorMessage implements the RPCData interface
 func (t TxnExecutionErrData) ErrorMessage() string {
 	return t.ExecutionError
-}
-
-// Object containing data about the token: contract address, number of decimals and current price in STRK
-type TokenData struct {
-	// Token contract address
-	TokenAddress *felt.Felt `json:"token_address"`
-	// The number of decimals of the token
-	Decimals uint8 `json:"decimals"`
-	// Price in STRK (in FRI units)
-	PriceInStrk string `json:"price_in_strk"` // u256 as a hex string
 }
