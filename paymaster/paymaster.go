fe1c2363b97cad542ab479b779ee4ef616499343
thiagodeev <thiagodeev@gmail.com>
2025-10-15 09:30:31 -0300

    fix: linter errors

--- paymaster/paymaster.go
+++ paymaster/paymaster.go
@@ -10,7 +10,8 @@ import (
 )
 
 // Paymaster is a client for interacting with a paymaster service via the SNIP-29 API.
-// It provides methods to build and execute transactions, check service status, and track transaction status.
+// It provides methods to build and execute transactions, check service
+// status, and track transaction status.
 type Paymaster struct {
 	// c is the underlying client for the paymaster service.
 	c callCloser
@@ -18,18 +19,25 @@ type Paymaster struct {
 
 // Used to assert that the Paymaster struct implements all the paymaster methods.
 // Ref: https://github.com/starknet-io/SNIPs/blob/ea46a8777d8c8d53a43f45b7beb1abcc301a1a69/assets/snip-29/paymaster_api.json
+//
+//nolint:lll // The link would be unclickable if we break the line.
 type paymasterInterface interface {
 	IsAvailable(ctx context.Context) (bool, error)
 	// More methods coming...
 }
 
-var _ paymasterInterface = &Paymaster{} //nolint:exhaustruct
+var _ paymasterInterface = (*Paymaster)(nil)
 
 // callCloser is an interface that defines the methods for calling a remote procedure.
 // It was created to match the Client struct from the 'client' package.
 type callCloser interface {
 	CallContext(ctx context.Context, result interface{}, method string, args interface{}) error
-	CallContextWithSliceArgs(ctx context.Context, result interface{}, method string, args ...interface{}) error
+	CallContextWithSliceArgs(
+		ctx context.Context,
+		result interface{},
+		method string,
+		args ...interface{},
+	) error
 	Close()
 }
 
@@ -48,7 +56,7 @@ func New(url string, options ...client.ClientOption) (*Paymaster, error) {
 	if err != nil {
 		return nil, err
 	}
-	httpClient := &http.Client{Jar: jar} //nolint:exhaustruct
+	httpClient := &http.Client{Jar: jar} //nolint:exhaustruct // Only the Jar field is used.
 	// prepend the custom client to allow users to override
 	options = append([]client.ClientOption{client.WithHTTPClient(httpClient)}, options...)
 	c, err := client.DialOptions(context.Background(), url, options...)
