ae1beeb5e21aa5c9b88016449f5f813315b13fb7
thiagodeev <thiagodeev@gmail.com>
2025-10-22 19:17:50 -0300

    docs: improve SpecVersion method description

--- rpc/version.go
+++ rpc/version.go
@@ -6,9 +6,16 @@ import (
 	"github.com/NethermindEth/starknet.go/client/rpcerr"
 )
 
-// SpecVersion returns the version of the Starknet JSON-RPC specification being used
-// Parameters: None
-// Returns: String of the Starknet JSON-RPC specification
+// SpecVersion returns the version of the Starknet JSON-RPC specification being
+// implemented by the node.
+//
+// Parameters:
+//   - ctx: The context for the function.
+//
+// Returns:
+//   - string: The version of the Starknet JSON-RPC specification
+//     implemented by the node.
+//   - error: An error if the request fails.
 func (provider *Provider) SpecVersion(ctx context.Context) (string, error) {
 	var result string
 	err := do(ctx, provider.c, "starknet_specVersion", &result)
