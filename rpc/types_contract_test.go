0b9c66f4f3d90d340ec9af2a939d8890f27e2cad
Thiago Ribeiro <62709592+thiagodeev@users.noreply.github.com>
2025-11-04 09:08:03 -0300

    Thiagodeev/fix-fee-limit (#819)
    * feat: new U128.ToBigInt() method
    
    * feat: make U128.ToBigInt() method validate max value + tests
    
    * test: add TestU128_ToUint64 test for the U64.ToUint64() method
    
    * refactor: update FeeEstToResBoundsMap to accept optional limit options
    
    - Modified the FeeEstToResBoundsMap function to include an optional parameter for custom limits on resource bounds.
    - Updated all relevant calls to FeeEstToResBoundsMap across transaction building functions and tests to accommodate the new parameter.
    - Enhanced error handling for gas price and amount limits to ensure proper validation and defaulting behavior.
    
    * test: enhance TestFeeEstToResBoundsMap with fee limit scenarios
    
    - Added new test cases to TestFeeEstToResBoundsMap to validate behavior with fee limits and multipliers.
    - Adjusted expected values to reflect accurate calculations based on the new fee limit logic.
    
    * test: fix TestResBoundsMapToOverallFee errors
    
    * chore: update changelog
    
    * docs: improve FeeEstToResBoundsMap description
    
    * chore: describe the U128.ToBigInt() method in the changelog
    
    * feat: new CustomFeeEstToResBoundsMap method
    
    * test: new CustomFeeEstToResBoundsMap test case
    
    - New test case for only one limit value provided, assuring no overflow above the u64 and u128 limits
    
    * chore: update changelog
    
    * chore: implement PR comments observations

---
+++ rpc/types_contract_test.go
@@ -0,0 +1,109 @@
+package rpc_test
+
+import (
+	"math"
+	"math/big"
+	"testing"
+
+	internalUtils "github.com/NethermindEth/starknet.go/internal/utils"
+	"github.com/NethermindEth/starknet.go/rpc"
+	"github.com/stretchr/testify/assert"
+)
+
+// TestU128_ToBigInt tests the ToBigInt method of the U128 type.
+func TestU128_ToBigInt(t *testing.T) {
+	tests := []struct {
+		name    string // description of this test case
+		u128    rpc.U128
+		want    *big.Int
+		wantErr bool
+	}{
+		{
+			name: "within the range",
+			u128: "0xabcdef",
+			want: internalUtils.HexToBN("0xabcdef"),
+		},
+		{
+			name: "max uint128",
+			u128: "0xffffffffffffffffffffffffffffffff",
+			want: internalUtils.HexToBN("0xffffffffffffffffffffffffffffffff"),
+		},
+		{
+			name:    "out of range",
+			u128:    "0x100000000000000000000000000000000",
+			wantErr: true,
+		},
+		{
+			name:    "invalid hex string",
+			u128:    "56yrty45",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			u := tt.u128
+			got, gotErr := u.ToBigInt()
+			if gotErr != nil {
+				if !tt.wantErr {
+					t.Errorf("ToBigInt() failed: %v", gotErr)
+				}
+
+				return
+			}
+			if tt.wantErr {
+				t.Fatal("ToBigInt() succeeded unexpectedly")
+			}
+
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+// TestU128_ToUint64 tests the ToUint64 method of the U128 type.
+func TestU128_ToUint64(t *testing.T) {
+	tests := []struct {
+		name    string // description of this test case
+		u64     rpc.U64
+		want    uint64
+		wantErr bool
+	}{
+		{
+			name: "within the range",
+			u64:  "0xabcdef",
+			want: 11259375,
+		},
+		{
+			name: "max uint64",
+			u64:  "0xFFFFFFFFFFFFFFFF",
+			want: math.MaxUint64,
+		},
+		{
+			name:    "out of range",
+			u64:     "0x10000000000000000",
+			wantErr: true,
+		},
+		{
+			name:    "invalid hex string",
+			u64:     "56yrty45",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			u := tt.u64
+			got, gotErr := u.ToUint64()
+			if gotErr != nil {
+				if !tt.wantErr {
+					t.Errorf("ToUint64() failed: %v", gotErr)
+				}
+
+				return
+			}
+			if tt.wantErr {
+				t.Fatal("ToUint64() succeeded unexpectedly")
+			}
+
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
