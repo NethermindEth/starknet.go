b899bd1e3492d6b354f6c82895e6f763262455f0
thiagodeev <thiagodeev@gmail.com>
2025-12-10 13:00:36 -0300

    test: update TestBlockTransactionCount to improve error handling and streamline test cases

--- rpc/block_test.go
+++ rpc/block_test.go
@@ -124,7 +124,7 @@ func TestBlockWithTxHashes(t *testing.T) {
 				BlockID: WithBlockTag(BlockTagPreConfirmed),
 			},
 			{
-				BlockID: WithBlockTag(BlockTagL1Accepted),
+				BlockID: WithBlockTag(BlockTagLatest),
 			},
 			{
 				BlockID:     WithBlockNumber(99999999999999999),
@@ -190,7 +190,7 @@ func TestBlockWithTxHashes(t *testing.T) {
 							switch blockID.Tag {
 							case BlockTagPreConfirmed:
 								*rawResp = blockSepoliaPreConfirmed
-							case BlockTagL1Accepted:
+							case BlockTagLatest:
 								*rawResp = blockSepolia3100000
 							}
 
@@ -210,7 +210,7 @@ func TestBlockWithTxHashes(t *testing.T) {
 			result, err := provider.BlockWithTxHashes(t.Context(), test.BlockID)
 			if test.ExpectedErr != nil {
 				require.Error(t, err)
-				require.ErrorContains(t, err, test.ExpectedErr.Error())
+				assert.EqualError(t, err, test.ExpectedErr.Error())
 
 				return
 			}
@@ -256,7 +256,7 @@ func TestBlockWithTxs(t *testing.T) {
 				BlockID: WithBlockTag(BlockTagPreConfirmed),
 			},
 			{
-				BlockID: WithBlockTag(BlockTagL1Accepted),
+				BlockID: WithBlockTag(BlockTagLatest),
 			},
 			{
 				BlockID:     WithBlockNumber(99999999999999999),
@@ -322,7 +322,7 @@ func TestBlockWithTxs(t *testing.T) {
 							switch blockID.Tag {
 							case BlockTagPreConfirmed:
 								*rawResp = blockSepoliaPreConfirmed
-							case BlockTagL1Accepted:
+							case BlockTagLatest:
 								*rawResp = blockSepolia3100000
 							}
 
@@ -367,100 +367,114 @@ func TestBlockWithTxs(t *testing.T) {
 	}
 }
 
-// TestBlockTransactionCount tests the function that calculates the number of transactions in a block.
+// TestBlockTransactionCount tests the BlockTransactionCount function.
 func TestBlockTransactionCount(t *testing.T) {
-	tests.RunTestOn(t, tests.MockEnv, tests.TestnetEnv, tests.IntegrationEnv)
+	tests.RunTestOn(t,
+		tests.IntegrationEnv,
+		tests.MainnetEnv,
+		tests.MockEnv,
+		tests.TestnetEnv,
+	)
 
 	testConfig := BeforeEach(t, false)
+	provider := testConfig.Provider
 
 	type testSetType struct {
-		BlockID       BlockID
-		ExpectedCount int64
-		ExpectedError error
+		BlockID     BlockID
+		ExpectedErr error
 	}
+
 	testSet := map[tests.TestEnv][]testSetType{
 		tests.MockEnv: {
 			{
-				BlockID:       WithBlockNumber(300000),
-				ExpectedCount: 10,
+				BlockID: WithBlockTag(BlockTagLatest),
 			},
 			{
-				BlockID:       WithBlockTag(BlockTagLatest),
-				ExpectedCount: 10,
-			},
-			{
-				BlockID:       WithBlockTag(BlockTagPreConfirmed),
-				ExpectedCount: 10,
-			},
-			{
-				BlockID:       WithBlockTag(BlockTagL1Accepted),
-				ExpectedCount: 10,
-			},
-		},
-		tests.TestnetEnv: {
-			{
-				BlockID:       WithBlockNumber(30000),
-				ExpectedCount: 4,
-			},
-			{
-				BlockID:       WithBlockNumber(52959),
-				ExpectedCount: 58,
-			},
-			{
-				BlockID:       WithBlockTag(BlockTagPreConfirmed),
-				ExpectedCount: -1,
-			},
-			{
-				BlockID:       WithBlockTag(BlockTagLatest),
-				ExpectedCount: -1,
-			},
-			{
-				BlockID:       WithBlockTag(BlockTagL1Accepted),
-				ExpectedCount: -1,
-			},
-			{
-				BlockID:       WithBlockNumber(7338746823462834783),
-				ExpectedError: ErrBlockNotFound,
+				BlockID:     WithBlockNumber(99999999999999999),
+				ExpectedErr: ErrBlockNotFound,
 			},
 		},
 		tests.IntegrationEnv: {
 			{
-				BlockID:       WithBlockNumber(30000),
-				ExpectedCount: 4,
+				BlockID:     WithBlockNumber(99999999999999999),
+				ExpectedErr: ErrBlockNotFound,
 			},
+		},
+		tests.MainnetEnv: {
 			{
-				BlockID:       WithBlockNumber(529590),
-				ExpectedCount: 6,
+				BlockID:     WithBlockNumber(99999999999999999),
+				ExpectedErr: ErrBlockNotFound,
 			},
+		},
+		tests.TestnetEnv: {
 			{
-				BlockID:       WithBlockNumber(7338746823462834783),
-				ExpectedError: ErrBlockNotFound,
+				BlockID:     WithBlockNumber(99999999999999999),
+				ExpectedErr: ErrBlockNotFound,
 			},
 		},
 	}[tests.TEST_ENV]
+
+	if tests.TEST_ENV != tests.MockEnv {
+		// add the common block IDs to the test set of network tests
+		blockIDs := GetCommonBlockIDs(t, provider)
+		for _, blockID := range blockIDs {
+			testSet = append(testSet, testSetType{
+				BlockID: blockID,
+			})
+		}
+	}
+
 	for _, test := range testSet {
-		t.Run(
-			fmt.Sprintf("Count: %v, BlockID: %v", test.ExpectedCount, test.BlockID),
-			func(t *testing.T) {
-				count, err := testConfig.Provider.BlockTransactionCount(
-					context.Background(),
-					test.BlockID,
-				)
-				if test.ExpectedError != nil {
-					require.EqualError(t, test.ExpectedError, err.Error())
+		blockID, _ := test.BlockID.MarshalJSON()
+		t.Run(fmt.Sprintf("BlockID: %v", string(blockID)), func(t *testing.T) {
+			if tests.TEST_ENV == tests.MockEnv {
+				testConfig.MockClient.EXPECT().
+					CallContextWithSliceArgs(
+						t.Context(),
+						gomock.Any(),
+						"starknet_getBlockTransactionCount",
+						test.BlockID,
+					).
+					DoAndReturn(
+						func(_, result, _ any, args ...any) error {
+							rawResp := result.(*json.RawMessage)
+							blockID := args[0].(BlockID)
 
-					return
-				}
-				require.NoError(t, err)
+							if blockID.Tag == BlockTagLatest {
+								*rawResp = json.RawMessage("100")
+							}
 
-				if test.ExpectedCount == -1 {
-					// since 0 is the default value of an int64 var, let's set the expected count to -1 when we want to skip the count check
-					return
-				}
+							if blockID.Number != nil && *blockID.Number == 99999999999999999 {
+								return RPCError{
+									Code:    24,
+									Message: "Block not found",
+								}
+							}
 
-				assert.Equal(t, uint64(test.ExpectedCount), count)
-			},
-		)
+							return nil
+						},
+					).
+					Times(1)
+			}
+
+			count, err := testConfig.Provider.BlockTransactionCount(
+				t.Context(),
+				test.BlockID,
+			)
+			if test.ExpectedErr != nil {
+				require.Error(t, err)
+				assert.EqualError(t, err, test.ExpectedErr.Error())
+
+				return
+			}
+			require.NoError(t, err)
+
+			rawExpectedCount := testConfig.Spy.LastResponse()
+
+			rawCount, err := json.Marshal(count)
+			require.NoError(t, err)
+			assert.JSONEq(t, string(rawExpectedCount), string(rawCount))
+		})
 	}
 }
 
