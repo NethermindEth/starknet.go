2683ee5ac7739d2899be852bbae9d2446ac6ff98
thiagodeev <thiagodeev@gmail.com>
2025-12-10 22:26:50 -0300

    test: refactor TestClassAt to enhance error handling and improve mock response management; remove no longer used data files

--- rpc/contract_test.go
+++ rpc/contract_test.go
@@ -13,128 +13,174 @@ import (
 	internalUtils "github.com/NethermindEth/starknet.go/internal/utils"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
 )
 
 // TestClassAt tests the ClassAt function.
-//
-// The function tests the ClassAt function by creating different test sets for different environments
-// (mock, testnet, and mainnet). It then iterates over each test set and performs the following steps:
-//   - Calls the ClassAt function with the specified block tag and contract address.
-//   - Checks the response type and performs the following actions based on the type:
-//   - If the response type is DeprecatedContractClass or ContractClass:
-//   - Checks if the program code exists in the response object.
-//   - Checks if the expected operation exist in the provided contract address
-//   - If the response type is of unknown type: log and fail the test
-//
-// Parameters:
-//   - t: the testing object for running the test cases
-//
-// Returns:
-//
-//	none
 func TestClassAt(t *testing.T) {
-	tests.RunTestOn(t, tests.MockEnv, tests.TestnetEnv, tests.MainnetEnv, tests.IntegrationEnv)
+	tests.RunTestOn(t,
+		tests.MockEnv,
+		tests.TestnetEnv,
+		tests.MainnetEnv,
+		tests.IntegrationEnv,
+	)
 
 	testConfig := BeforeEach(t, false)
 
 	type testSetType struct {
-		ContractAddress   *felt.Felt
-		ExpectedOperation string
-		Block             BlockID
+		Description     string
+		ContractAddress *felt.Felt
+		Block           BlockID
+		ExpectedError   error
 	}
 	testSet := map[tests.TestEnv][]testSetType{
 		tests.MockEnv: {
 			{
-				ContractAddress:   internalUtils.DeadBeef,
-				ExpectedOperation: internalUtils.DeadBeef.String(),
-				Block:             WithBlockNumber(58344),
+				Description:     "deprecated class",
+				ContractAddress: internalUtils.TestHexToFelt(t, "0x123"),
+				Block:           WithBlockTag(BlockTagLatest),
+			},
+			{
+				Description:     "sierra class",
+				ContractAddress: internalUtils.TestHexToFelt(t, "0x456"),
+				Block:           WithBlockTag(BlockTagLatest),
+			},
+			{
+				Description:     "invalid block",
+				ContractAddress: internalUtils.TestHexToFelt(t, "0x789"),
+				Block:           WithBlockHash(internalUtils.DeadBeef),
+				ExpectedError:   ErrBlockNotFound,
+			},
+			{
+				Description:     "invalid class hash",
+				ContractAddress: internalUtils.DeadBeef,
+				Block:           WithBlockTag(BlockTagLatest),
+				ExpectedError:   ErrContractNotFound,
 			},
 		},
 		tests.TestnetEnv: {
-			// v0 contract
 			{
-				ContractAddress:   internalUtils.TestHexToFelt(t, "0x073ad76dCF68168cBF68EA3EC0382a3605F3dEAf24dc076C355e275769b3c561"),
-				ExpectedOperation: internalUtils.GetSelectorFromNameFelt("getPublicKey").String(),
-				Block:             WithBlockNumber(58344),
-			},
-			// v2 contract
-			{
-				ContractAddress:   internalUtils.TestHexToFelt(t, "0x04dAadB9d30c887E1ab2cf7D78DFE444A77AAB5a49C3353d6d9977e7eD669902"),
-				ExpectedOperation: internalUtils.GetSelectorFromNameFelt("name_get").String(),
-				Block:             WithBlockNumber(65168),
+				Description:     "deprecated class",
+				ContractAddress: internalUtils.TestHexToFelt(t, "0x073ad76dCF68168cBF68EA3EC0382a3605F3dEAf24dc076C355e275769b3c561"),
+				Block:           WithBlockTag(BlockTagLatest),
 			},
 			{
-				ContractAddress:   internalUtils.TestHexToFelt(t, "0x04dAadB9d30c887E1ab2cf7D78DFE444A77AAB5a49C3353d6d9977e7eD669902"),
-				ExpectedOperation: internalUtils.GetSelectorFromNameFelt("name_get").String(),
-				Block:             WithBlockTag(BlockTagPreConfirmed),
+				Description:     "sierra class",
+				ContractAddress: internalUtils.TestHexToFelt(t, "0x04dAadB9d30c887E1ab2cf7D78DFE444A77AAB5a49C3353d6d9977e7eD669902"),
+				Block:           WithBlockTag(BlockTagLatest),
 			},
 			{
-				ContractAddress:   internalUtils.TestHexToFelt(t, "0x04dAadB9d30c887E1ab2cf7D78DFE444A77AAB5a49C3353d6d9977e7eD669902"),
-				ExpectedOperation: internalUtils.GetSelectorFromNameFelt("name_get").String(),
-				Block:             WithBlockTag(BlockTagLatest),
+				Description:     "invalid contract",
+				ContractAddress: internalUtils.DeadBeef,
+				Block:           WithBlockTag(BlockTagLatest),
+				ExpectedError:   ErrContractNotFound,
 			},
 			{
-				ContractAddress:   internalUtils.TestHexToFelt(t, "0x04dAadB9d30c887E1ab2cf7D78DFE444A77AAB5a49C3353d6d9977e7eD669902"),
-				ExpectedOperation: internalUtils.GetSelectorFromNameFelt("name_get").String(),
-				Block:             WithBlockTag(BlockTagL1Accepted),
+				Description:     "invalid block",
+				ContractAddress: internalUtils.TestHexToFelt(t, "0x04dAadB9d30c887E1ab2cf7D78DFE444A77AAB5a49C3353d6d9977e7eD669902"),
+				Block:           WithBlockHash(internalUtils.DeadBeef),
+				ExpectedError:   ErrBlockNotFound,
 			},
 		},
 		tests.IntegrationEnv: {
 			{
-				ContractAddress:   internalUtils.TestHexToFelt(t, "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d"),
-				ExpectedOperation: internalUtils.GetSelectorFromNameFelt("decimals").String(),
-				Block:             WithBlockNumber(643360),
+				Description:     "sierra class",
+				ContractAddress: internalUtils.TestHexToFelt(t, "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d"),
+				Block:           WithBlockTag(BlockTagLatest),
 			},
 		},
 		tests.MainnetEnv: {
 			{
-				ContractAddress:   internalUtils.TestHexToFelt(t, "0x004b3d247e79c58e77c93e2c52025d0bb1727957cc9c33b33f7216f369c77be5"),
-				ExpectedOperation: internalUtils.GetSelectorFromNameFelt("get_name").String(),
-				Block:             WithBlockNumber(643360),
+				Description:     "sierra class",
+				ContractAddress: internalUtils.TestHexToFelt(t, "0x004b3d247e79c58e77c93e2c52025d0bb1727957cc9c33b33f7216f369c77be5"),
+				Block:           WithBlockTag(BlockTagLatest),
 			},
 		},
 	}[tests.TEST_ENV]
 
 	for _, test := range testSet {
-		t.Run(
-			fmt.Sprintf("BlockID: %v, ContractAddress: %v", test.Block, test.ContractAddress),
-			func(t *testing.T) {
-				resp, err := testConfig.Provider.ClassAt(
-					context.Background(),
-					test.Block,
-					test.ContractAddress,
-				)
+		t.Run(test.Description, func(t *testing.T) {
+			if tests.TEST_ENV == tests.MockEnv {
+				testConfig.MockClient.EXPECT().
+					CallContextWithSliceArgs(
+						t.Context(),
+						gomock.Any(),
+						"starknet_getClassAt",
+						test.Block,
+						test.ContractAddress,
+					).
+					DoAndReturn(func(_, result, _ any, args ...any) error {
+						rawResp := result.(*json.RawMessage)
+						blockID := args[0].(BlockID)
+						contractAddress := args[1].(*felt.Felt)
+
+						if blockID.Hash != nil && blockID.Hash == internalUtils.DeadBeef {
+							return RPCError{
+								Code:    24,
+								Message: "Block not found",
+							}
+						}
+
+						if contractAddress == internalUtils.DeadBeef {
+							return RPCError{
+								Code:    20,
+								Message: "Contract not found",
+							}
+						}
+
+						var class json.RawMessage
+						if contractAddress.String() == "0x123" {
+							// deprecated class
+							class = *internalUtils.TestUnmarshalJSONFileToType[json.RawMessage](
+								t,
+								"testData/class/0x036c7e49a16f8fc760a6fbdf71dde543d98be1fee2eda5daff59a0eeae066ed9.json",
+								"result",
+							)
+						}
+						if contractAddress.String() == "0x456" {
+							// sierra class
+							class = *internalUtils.TestUnmarshalJSONFileToType[json.RawMessage](
+								t,
+								"testData/class/0x01f372292df22d28f2d4c5798734421afe9596e6a566b8bc9b7b50e26521b855.json",
+								"result",
+							)
+						}
+
+						*rawResp = class
+
+						return nil
+					}).
+					Times(1)
+			}
+
+			resp, err := testConfig.Provider.ClassAt(
+				t.Context(),
+				test.Block,
+				test.ContractAddress,
+			)
+			if test.ExpectedError != nil {
+				require.Error(t, err)
+				assert.EqualError(t, err, test.ExpectedError.Error())
+
+				return
+			}
+			require.NoError(t, err)
+
+			rawExpectedResp := testConfig.Spy.LastResponse()
+
+			switch class := resp.(type) {
+			case *contracts.DeprecatedContractClass:
+				rawClass, err := json.Marshal(class)
 				require.NoError(t, err)
-
-				switch class := resp.(type) {
-				case *contracts.DeprecatedContractClass:
-					require.NotEmpty(t, class.Program, "code should exist")
-
-					assert.Condition(t, func() bool {
-						for _, deprecatedCairoEntryPoint := range class.DeprecatedEntryPointsByType.External {
-							if test.ExpectedOperation == deprecatedCairoEntryPoint.Selector.String() {
-								return true
-							}
-						}
-
-						return false
-					}, "operation not found in the class")
-				case *contracts.ContractClass:
-					require.NotEmpty(t, class.SierraProgram, "code should exist")
-
-					assert.Condition(t, func() bool {
-						for _, entryPointsByType := range class.EntryPointsByType.External {
-							if test.ExpectedOperation == entryPointsByType.Selector.String() {
-								return true
-							}
-						}
-
-						return false
-					}, "operation not found in the class")
-				default:
-					t.Fatalf("Received unknown response type: %v", reflect.TypeOf(resp))
-				}
-			},
+				assert.JSONEq(t, string(rawExpectedResp), string(rawClass))
+			case *contracts.ContractClass:
+				rawClass, err := json.Marshal(class)
+				require.NoError(t, err)
+				assert.JSONEq(t, string(rawExpectedResp), string(rawClass))
+			default:
+				t.Fatalf("Received unknown response type: %v", reflect.TypeOf(resp))
+			}
+		},
 		)
 	}
 }
