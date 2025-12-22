9faa8d83938624c322a3194edd1bb58b2115be3f
thiagodeev <thiagodeev@gmail.com>
2025-12-22 14:49:32 -0300

    test: refactor TestTransactionTrace test

--- rpc/trace_test.go
+++ rpc/trace_test.go
@@ -11,94 +11,120 @@ import (
 	internalUtils "github.com/NethermindEth/starknet.go/internal/utils"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
 )
 
-// TestTransactionTrace is a function that tests the TransactionTrace function.
-//
-// It sets up the necessary test configuration and expected response. Then it performs a series of test sets,
-// each with a different transaction hash. For each test set, it calls the TransactionTrace function and compares
-// the response with the expected response.
-//
-// Parameters:
-//   - t: the testing object for running the test cases
-//
-// Returns:
-//
-//	none
+// TestTraceTransaction tests the TraceTransaction function.
 func TestTransactionTrace(t *testing.T) {
 	tests.RunTestOn(t, tests.MockEnv, tests.TestnetEnv, tests.IntegrationEnv)
 	testConfig := BeforeEach(t, false)
 
-	expectedFile1 := "./testData/trace/sepoliaInvokeTrace_0x6a4a9c4f1a530f7d6dd7bba9b71f090a70d1e3bbde80998fde11a08aab8b282.json"
-
 	type testSetType struct {
-		TransactionHash  *felt.Felt
-		ExpectedRespFile string
-		ExpectedError    error
+		TransactionHash *felt.Felt
+		ExpectedError   error
 	}
 	testSet := map[tests.TestEnv][]testSetType{
 		tests.MockEnv: {
-			testSetType{
-				TransactionHash:  internalUtils.TestHexToFelt(t, "0x6a4a9c4f1a530f7d6dd7bba9b71f090a70d1e3bbde80998fde11a08aab8b282"),
-				ExpectedRespFile: expectedFile1,
-				ExpectedError:    nil,
+			{
+				TransactionHash: internalUtils.TestHexToFelt(t, "0x6a4a9c4f1a530f7d6dd7bba9b71f090a70d1e3bbde80998fde11a08aab8b282"),
 			},
-			testSetType{
-				TransactionHash:  internalUtils.TestHexToFelt(t, "0xc0ffee"),
-				ExpectedRespFile: expectedFile1,
-				ExpectedError:    ErrHashNotFound,
+			{
+				TransactionHash: internalUtils.DeadBeef,
+				ExpectedError:   ErrHashNotFound,
 			},
-			testSetType{
-				TransactionHash:  internalUtils.TestHexToFelt(t, "0xf00d"),
-				ExpectedRespFile: expectedFile1,
+			{
+				TransactionHash: &felt.Zero,
 				ExpectedError: &RPCError{
 					Code:    10,
 					Message: "No trace available for transaction",
-					Data:    &TraceStatusErrData{Status: TraceStatusRejected},
+					Data: &TraceStatusErrData{
+						Status: TraceStatusReceived,
+					},
 				},
 			},
 		},
 		tests.TestnetEnv: {
-			testSetType{ // with 5 out of 6 fields (without state diff)
-				TransactionHash:  internalUtils.TestHexToFelt(t, "0x6a4a9c4f1a530f7d6dd7bba9b71f090a70d1e3bbde80998fde11a08aab8b282"),
-				ExpectedRespFile: expectedFile1,
-				ExpectedError:    nil,
+			{ // with 5 out of 6 fields (without state diff)
+				TransactionHash: internalUtils.TestHexToFelt(t, "0x6a4a9c4f1a530f7d6dd7bba9b71f090a70d1e3bbde80998fde11a08aab8b282"),
 			},
-			testSetType{ // with 6 out of 6 fields
-				TransactionHash:  internalUtils.TestHexToFelt(t, "0x49d98a0328fee1de19d43d950cbaeb973d080d0c74c652523371e034cc0bbb2"),
-				ExpectedRespFile: "./testData/trace/sepoliaInvokeTrace_0x49d98a0328fee1de19d43d950cbaeb973d080d0c74c652523371e034cc0bbb2.json",
-				ExpectedError:    nil,
+			{ // with 6 out of 6 fields
+				TransactionHash: internalUtils.TestHexToFelt(t, "0x49d98a0328fee1de19d43d950cbaeb973d080d0c74c652523371e034cc0bbb2"),
+			},
+			{
+				TransactionHash: internalUtils.DeadBeef,
+				ExpectedError:   ErrHashNotFound,
 			},
 		},
 		tests.IntegrationEnv: {
-			testSetType{
-				TransactionHash:  internalUtils.TestHexToFelt(t, "0x38f7c9972f2b6f6d92d474cf605a077d154d58de938125180e7c87f22c5b019"),
-				ExpectedRespFile: "./testData/trace/integrationInvokeTrace_0x38f7c9972f2b6f6d92d474cf605a077d154d58de938125180e7c87f22c5b019.json",
-				ExpectedError:    nil,
+			{
+				TransactionHash: internalUtils.TestHexToFelt(t, "0x38f7c9972f2b6f6d92d474cf605a077d154d58de938125180e7c87f22c5b019"),
+			},
+			{
+				TransactionHash: internalUtils.DeadBeef,
+				ExpectedError:   ErrHashNotFound,
 			},
 		},
 	}[tests.TEST_ENV]
 
 	for _, test := range testSet {
 		t.Run(test.TransactionHash.String(), func(t *testing.T) {
-			expectedResp := *internalUtils.TestUnmarshalJSONFileToType[InvokeTxnTrace](t, test.ExpectedRespFile, "result")
+			if tests.TEST_ENV == tests.MockEnv {
+					testConfig.MockClient.EXPECT().
+						CallContextWithSliceArgs(
+							t.Context(),
+							gomock.Any(),
+							"starknet_traceTransaction",
+							test.TransactionHash,
+						).
+					DoAndReturn(func(_, result, _ any, args ...any) error {
+						rawResp := result.(*json.RawMessage)
+						transactionHash := args[0].(*felt.Felt)
+
+						if transactionHash == internalUtils.DeadBeef {
+							return RPCError{
+								Code:    29,
+								Message: "Transaction hash not found",
+							}
+						}
+
+						if *transactionHash == felt.Zero {
+							return RPCError{
+								Code:    10,
+								Message: "No trace available for transaction",
+								Data: &TraceStatusErrData{
+									Status: TraceStatusReceived,
+								},
+							}
+						}
+
+						*rawResp = *internalUtils.TestUnmarshalJSONFileToType[json.RawMessage](
+						t,
+						"./testData/trace/sepoliaInvokeTrace_0x6a4a9c4f1a530f7d6dd7bba9b71f090a70d1e3bbde80998fde11a08aab8b282.json",
+						"result",
+					)
+
+							return nil
+						}).
+						Times(1)
+				}
 
 			resp, err := testConfig.Provider.TraceTransaction(
-				context.Background(),
+				t.Context(),
 				test.TransactionHash,
 			)
 			if test.ExpectedError != nil {
-				assert.EqualError(t, test.ExpectedError, err.Error())
+				require.Error(t, err)
+				assert.EqualError(t, err, test.ExpectedError.Error())
 
 				return
 			}
-			compareTraceTxs(t, expectedResp, resp)
+			require.NoError(t, err)
 
+			rawExpectedResp := testConfig.Spy.LastResponse()
 			rawResp, err := json.Marshal(resp)
 			require.NoError(t, err)
-			rawExpectedResp := *internalUtils.TestUnmarshalJSONFileToType[json.RawMessage](t, test.ExpectedRespFile, "result")
 
-			compareTraceTxnsJSON(t, rawExpectedResp, rawResp)
+			assert.JSONEq(t, string(rawExpectedResp), string(rawResp))
 		})
 	}
 }
