8142d48af352d079409959ea8d3e9ff25c8f134e
thiagodeev <thiagodeev@gmail.com>
2025-12-23 15:40:32 -0300

    test: refactor TestTransactionReceipt test

--- rpc/transaction_test.go
+++ rpc/transaction_test.go
@@ -223,55 +223,95 @@ func TestTransactionByBlockIdAndIndex(t *testing.T) {
 	}
 }
 
+// TestTransactionReceipt tests the TransactionReceipt function.
 func TestTransactionReceipt(t *testing.T) {
 	tests.RunTestOn(t, tests.MockEnv, tests.TestnetEnv, tests.IntegrationEnv)
 
 	testConfig := BeforeEach(t, false)
 
 	type testSetType struct {
-		TxnHash      *felt.Felt
-		ExpectedResp TransactionReceiptWithBlockInfo
+		TxnHash       *felt.Felt
+		ExpectedError error
 	}
-
-	receiptTxn52767_16 := *internalUtils.TestUnmarshalJSONFileToType[TransactionReceiptWithBlockInfo](t, "./testData/receipt/sepoliaRec_0xf2f3d50192637e8d5e817363460c39d3a668fe12f117ecedb9749466d8352b.json", "")
-
-	integrationInvokeV3Example := *internalUtils.TestUnmarshalJSONFileToType[TransactionReceiptWithBlockInfo](t, "./testData/receipt/integration_0x38f7c9972f2b6f6d92d474cf605a077d154d58de938125180e7c87f22c5b019.json", "result")
-
-	// https://voyager.online/tx/0x74011377f326265f5a54e27a27968355e7033ad1de11b77b225374875aff519
-	receiptL1Handler := *internalUtils.TestUnmarshalJSONFileToType[TransactionReceiptWithBlockInfo](t, "./testData/receipt/mainnetRc_0x74011377f326265f5a54e27a27968355e7033ad1de11b77b225374875aff519.json", "")
 
 	testSet := map[tests.TestEnv][]testSetType{
 		tests.MockEnv: {
 			{
-				TxnHash:      internalUtils.TestHexToFelt(t, "0xf2f3d50192637e8d5e817363460c39d3a668fe12f117ecedb9749466d8352b"),
-				ExpectedResp: receiptTxn52767_16,
+				TxnHash: internalUtils.TestHexToFelt(t, "0xf2f3d50192637e8d5e817363460c39d3a668fe12f117ecedb9749466d8352b"),
 			},
 			{
-				TxnHash:      internalUtils.TestHexToFelt(t, "0x74011377f326265f5a54e27a27968355e7033ad1de11b77b225374875aff519"),
-				ExpectedResp: receiptL1Handler,
+				TxnHash:       internalUtils.DeadBeef,
+				ExpectedError: ErrHashNotFound,
 			},
 		},
 		tests.TestnetEnv: {
 			{
-				TxnHash:      internalUtils.TestHexToFelt(t, "0xf2f3d50192637e8d5e817363460c39d3a668fe12f117ecedb9749466d8352b"),
-				ExpectedResp: receiptTxn52767_16,
+				TxnHash: internalUtils.TestHexToFelt(t, "0xf2f3d50192637e8d5e817363460c39d3a668fe12f117ecedb9749466d8352b"),
+			},
+			{
+				TxnHash:       internalUtils.DeadBeef,
+				ExpectedError: ErrHashNotFound,
 			},
 		},
 		tests.IntegrationEnv: {
 			{
-				TxnHash:      internalUtils.TestHexToFelt(t, "0x38f7c9972f2b6f6d92d474cf605a077d154d58de938125180e7c87f22c5b019"),
-				ExpectedResp: integrationInvokeV3Example,
+				TxnHash: internalUtils.TestHexToFelt(t, "0x38f7c9972f2b6f6d92d474cf605a077d154d58de938125180e7c87f22c5b019"),
+			},
+			{
+				TxnHash:       internalUtils.DeadBeef,
+				ExpectedError: ErrHashNotFound,
 			},
 		},
 	}[tests.TEST_ENV]
 
 	for _, test := range testSet {
+		t.Run(test.TxnHash.String(), func(t *testing.T) {
+			if tests.TEST_ENV == tests.MockEnv {
+				testConfig.MockClient.EXPECT().
+					CallContextWithSliceArgs(
+						t.Context(),
+						gomock.Any(),
+						"starknet_getTransactionReceipt",
+						test.TxnHash,
+					).
+					DoAndReturn(func(_, result, _ any, _ ...any) error {
+						rawResp := result.(*json.RawMessage)
+
+						if test.TxnHash == internalUtils.DeadBeef {
+							return RPCError{
+								Code:    29,
+								Message: "Transaction hash not found",
+							}
+						}
+
+						*rawResp = *internalUtils.TestUnmarshalJSONFileToType[json.RawMessage](
+							t,
+							"./testData/receipt/sepoliaReceipt.json",
+							"result",
+						)
+
+						return nil
+					}).
+					Times(1)
+			}
+
 		txReceiptWithBlockInfo, err := testConfig.Provider.TransactionReceipt(
-			context.Background(),
+				t.Context(),
 			test.TxnHash,
 		)
-		require.Nil(t, err)
-		require.Equal(t, test.ExpectedResp, *txReceiptWithBlockInfo)
+			if test.ExpectedError != nil {
+				require.Error(t, err)
+				assert.EqualError(t, err, test.ExpectedError.Error())
+
+				return
+			}
+			require.NoError(t, err)
+
+			rawExpectedResp := testConfig.Spy.LastResponse()
+			rawReceipt, err := json.Marshal(txReceiptWithBlockInfo)
+			require.NoError(t, err)
+			assert.JSONEq(t, string(rawExpectedResp), string(rawReceipt))
+		})
 	}
 }
 
