6b720ad6680ac349e5d7d8ad770d4aab40bae437
thiagodeev <thiagodeev@gmail.com>
2025-12-23 16:33:29 -0300

    test: refactor TestRPCError test

--- rpc/errors_test.go
+++ rpc/errors_test.go
@@ -1,58 +1,67 @@
 package rpc
 
 import (
-	"context"
+	"encoding/json"
 	"strconv"
 	"testing"
 
 	"github.com/NethermindEth/juno/core/felt"
-	"github.com/NethermindEth/starknet.go/client/rpcerr"
 	"github.com/NethermindEth/starknet.go/internal/tests"
 	internalUtils "github.com/NethermindEth/starknet.go/internal/utils"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
 )
 
+// TestRPCError tests the RPCError type, checking if a given error is unwrapped correctly.
 func TestRPCError(t *testing.T) {
 	tests.RunTestOn(t, tests.MockEnv, tests.TestnetEnv, tests.IntegrationEnv)
 
-	if tests.TEST_ENV == tests.MockEnv {
-		testConfig := BeforeEach(t, false)
-		_, err := testConfig.Provider.ChainID(context.Background())
-		require.NoError(t, err)
-
-		_, err = testConfig.Provider.Events(
-			context.Background(),
-			EventsInput{ResultPageRequest: ResultPageRequest{ChunkSize: 0}},
-		)
-		require.Error(t, err)
-		rpcErr := err.(*RPCError)
-		assert.Equal(t, rpcErr.Code, rpcerr.InternalError)
-		assert.NotNil(t, rpcErr.Message, "Internal Error")
-		assert.NotNil(t, rpcErr.Data, "-ChuckSize error message-")
-
-		assert.ErrorContains(t, err, strconv.Itoa(rpcErr.Code))
-		assert.ErrorContains(t, err, rpcErr.Message)
-		assert.ErrorContains(t, err, rpcErr.Data.ErrorMessage())
-
-		return
-	}
-
 	testConfig := BeforeEach(t, false)
 
-	// invalid msg
+	// invalid msg - empty payload
 	msgFromL1 := MsgFromL1{
 		FromAddress: "0x8453fc6cd1bcfe8d4dfc069c400b433054d47bdc",
 		ToAddress: internalUtils.TestHexToFelt(
 			t,
 			"0x04c5772d1914fe6ce891b64eb35bf3522aeae1315647314aac58b01137607f3f",
 		),
-		Selector: internalUtils.DeadBeef,
-		Payload:  []*felt.Felt{},
+		Selector: internalUtils.TestHexToFelt(
+			t,
+			"0x1b64b1b3b690b43b9b514fb81377518f4039cd3e4f4914d8a6bdf01d679fb19",
+		),
+		Payload: []*felt.Felt{},
+	}
+
+	if tests.TEST_ENV == tests.MockEnv {
+		testConfig.MockClient.EXPECT().
+			CallContextWithSliceArgs(
+				t.Context(),
+				gomock.Any(),
+				"starknet_estimateMessageFee",
+				msgFromL1,
+				WithBlockNumber(523066),
+			).
+			DoAndReturn(func(_, result, _ any, args ...any) error {
+				rpcErr := internalUtils.TestUnmarshalJSONFileToType[RPCError](
+					t,
+					"./testData/errors/contractError.json",
+					"error",
+				)
+				rawData, ok := rpcErr.Data.(StringErrData)
+				require.True(t, ok)
+				var contractErrData ContractErrData
+				err := json.Unmarshal([]byte(rawData), &contractErrData)
+				require.NoError(t, err)
+				rpcErr.Data = &contractErrData
+
+				return rpcErr
+			}).
+			Times(1)
 	}
 
 	_, err := testConfig.Provider.EstimateMessageFee(
-		context.Background(),
+		t.Context(),
 		msgFromL1,
 		WithBlockNumber(523066),
 	)
