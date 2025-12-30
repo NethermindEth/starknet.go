d6b524bd8a06c6105b0f739844c0b98087a8e08b
thiagodeev <thiagodeev@gmail.com>
2025-12-30 11:48:25 -0300

    test: finish mock tests for TestSubscribeTransactionStatus test

--- rpc/websocket_test.go
+++ rpc/websocket_test.go
@@ -994,7 +994,7 @@ func TestSubscribeTransactionStatus(t *testing.T) {
 		tests.TestnetEnv,
 	)
 
-	t.Run("network tests", func(t *testing.T) {
+	t.Run("network test", func(t *testing.T) {
 		tests.RunTestOn(t,
 			tests.IntegrationEnv,
 			tests.MainnetEnv,
@@ -1045,6 +1045,125 @@ func TestSubscribeTransactionStatus(t *testing.T) {
 
 				return
 			}
+		}
+	})
+
+	t.Run("mock tests", func(t *testing.T) {
+		tests.RunTestOn(t, tests.MockEnv)
+
+		testSet := []*felt.Felt{
+			new(felt.Felt).SetUint64(1), // RECEIVED
+			new(felt.Felt).SetUint64(2), // CANDIDATE
+			new(felt.Felt).SetUint64(3), // PRE_CONFIRMED
+			new(felt.Felt).SetUint64(4), // ACCEPTED_ON_L2
+			new(felt.Felt).SetUint64(5), // ACCEPTED_ON_L1
+			new(felt.Felt).SetUint64(6), // REVERTED
+		}
+
+		for _, hash := range testSet {
+			t.Run(hash.String(), func(t *testing.T) {
+				t.Parallel()
+				tsetup := BeforeEach(t, true)
+
+				tsetup.MockClient.EXPECT().
+					SubscribeWithSliceArgs(
+						t.Context(),
+						"starknet",
+						"_subscribeTransactionStatus",
+						gomock.Any(),
+						gomock.Any(),
+					).
+					DoAndReturn(func(_, _, _, channel any, args ...any) (*client.ClientSubscription, error) {
+						ch := channel.(chan json.RawMessage)
+						hash := args[0].(*felt.Felt)
+						var msg string
+
+						switch hash.Uint64() {
+						case 1:
+							msg = `{
+								"transaction_hash": "0x7ef307fc37cf8cfec75ba0ac88fa347bb6e4ff0b0937d45d64bde1cbe05a95",
+								"status": {
+									"finality_status": "RECEIVED",
+									"execution_status": "SUCCEEDED"
+								}}`
+						case 2:
+							msg = `{
+								"transaction_hash": "0x7ef307fc37cf8cfec75ba0ac88fa347bb6e4ff0b0937d45d64bde1cbe05a95",
+								"status": {
+									"finality_status": "CANDIDATE",
+									"execution_status": "SUCCEEDED"
+								}}`
+						case 3:
+							msg = `{
+								"transaction_hash": "0x7ef307fc37cf8cfec75ba0ac88fa347bb6e4ff0b0937d45d64bde1cbe05a95",
+								"status": {
+									"finality_status": "PRE_CONFIRMED",
+									"execution_status": "SUCCEEDED"
+								}}`
+						case 4:
+							msg = `{
+								"transaction_hash": "0x7ef307fc37cf8cfec75ba0ac88fa347bb6e4ff0b0937d45d64bde1cbe05a95",
+								"status": {
+									"finality_status": "ACCEPTED_ON_L2",
+									"execution_status": "SUCCEEDED"
+								}}`
+						case 5:
+							msg = `{
+								"transaction_hash": "0x7ef307fc37cf8cfec75ba0ac88fa347bb6e4ff0b0937d45d64bde1cbe05a95",
+								"status": {
+									"finality_status": "ACCEPTED_ON_L1",
+									"execution_status": "SUCCEEDED"
+								}}`
+						case 6:
+							rawMsg := internalUtils.TestUnmarshalJSONFileToType[json.RawMessage](
+								t,
+								"./testData/ws/mainnetTxnStatus.json",
+								"params", "result",
+							)
+							msg = string(rawMsg)
+						}
+
+						go func() {
+							for {
+								select {
+								case <-time.Tick(2 * time.Second):
+									ch <- json.RawMessage(msg)
+								case <-t.Context().Done():
+									return
+								}
+							}
+						}()
+
+						return &client.ClientSubscription{}, nil
+					}).
+					Times(1)
+
+				status := make(chan *NewTxnStatus)
+				sub, err := tsetup.WsProvider.SubscribeTransactionStatus(
+					t.Context(),
+					status,
+					hash,
+				)
+				require.NoError(t, err)
+
+				select {
+				case status := <-status:
+					rawExpectedMsg := <-tsetup.WSSpy.SpyChannel()
+					rawMsg, err := json.Marshal(status)
+					require.NoError(t, err)
+					assert.JSONEq(t, string(rawExpectedMsg), string(rawMsg))
+
+					return
+				case err := <-sub.Err():
+					require.NoError(t, err)
+
+					return
+				case <-time.After(testDuration * 2):
+					t.Fatal("timeout waiting for status")
+
+					return
+				}
+			})
 		}
 	})
 }
