22593ab5ddc70d93c5c0941f40f1298b13aadd68
thiagodeev <thiagodeev@gmail.com>
2026-01-05 14:49:17 -0300

    refactor: export Do func + new Caller interfaces

--- rpc/client.go
+++ rpc/client.go
@@ -2,16 +2,20 @@ package rpc
 
 import (
 	"context"
-	"encoding/json"
-	"errors"
+	"net/http"
+	"net/http/cookiejar"
 
 	"github.com/NethermindEth/starknet.go/client"
+	"golang.org/x/net/publicsuffix"
 )
 
-type callCloser interface {
+// @todo add description
+// @new
+type Caller interface {
 	// CallContext calls the RPC method with the specified parameters and
 	// returns an error.
 	CallContext(ctx context.Context, result interface{}, method string, args interface{}) error
+	// @todo remove quotes about Juno, same for the other interfaces
 	// CallContextWithSliceArgs call 'CallContext' with a slice of arguments.
 	// For RPC-Calls with optional arguments, use 'CallContext' instead and
 	// pass a struct containing the arguments, because Juno doesn't support
@@ -25,7 +29,9 @@ type callCloser interface {
 	Close()
 }
 
-type wsConn interface {
+// @todo add description
+// @new
+type Subscriber interface {
 	// Subscribe calls the "<namespace>_subscribe" method with the given arguments,
 	// registering a subscription. Server notifications for the subscription are
 	// sent to the given channel. The element type of the channel must match the
@@ -51,67 +57,23 @@ type wsConn interface {
 	Close()
 }
 
-// do is a function that performs a remote procedure call (RPC) using the
-// provided callCloser.
-// It passes the parameters as an array in the JSON-RPC call.
-//
-// Parameters:
-//   - ctx: represents the current execution context
-//   - call: the callCloser object
-//   - method: the string representing the RPC method to be called
-//   - data: the interface{} to store the result of the RPC call
-//   - args: variadic and can be used to pass additional arguments to the RPC method
-//
-// Returns:
-//   - error: an error if any occurred during the function call
-func do(
+// @new
+func NewClient(
 	ctx context.Context,
-	call callCloser,
-	method string,
-	data interface{},
-	args ...interface{},
-) error {
-	var raw json.RawMessage
-	err := call.CallContextWithSliceArgs(ctx, &raw, method, args...)
+	url string,
+	options ...client.ClientOption,
+) (*client.Client, error) {
+	jar, err := cookiejar.New(&cookiejar.Options{PublicSuffixList: publicsuffix.List})
 	if err != nil {
-		return err
+		return nil, err
 	}
-	if len(raw) == 0 {
-		return errors.New("not found")
-	}
-	if err := json.Unmarshal(raw, &data); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// doAsObject is a function that performs a remote procedure call (RPC) using
-// the provided callCloser. It passes the parameter as an object in the JSON-RPC
-// call, used for RPC-Calls with optional arguments since Juno doesn't support
-// optional arguments being passed in an array, only within an object.
-//
-// Parameters:
-//   - ctx: represents the current execution context
-//   - call: the callCloser object
-//   - method: the string representing the RPC method to be called
-//   - data: the interface{} to store the result of the RPC call
-//   - arg: the interface{} to pass as an object to the RPC method
-//
-// Returns:
-//   - error: an error if any occurred during the function call
-func doAsObject(ctx context.Context, call callCloser, method string, data, arg interface{}) error {
-	var raw json.RawMessage
-	err := call.CallContext(ctx, &raw, method, arg)
+	httpClient := &http.Client{Jar: jar} //nolint:exhaustruct // Only the Jar field is used.
+	// prepend the custom client to allow users to override
+	options = append([]client.ClientOption{client.WithHTTPClient(httpClient)}, options...)
+	c, err := client.DialOptions(ctx, url, options...)
 	if err != nil {
-		return err
-	}
-	if len(raw) == 0 {
-		return errors.New("not found")
-	}
-	if err := json.Unmarshal(raw, &data); err != nil {
-		return err
+		return nil, err
 	}
 
-	return nil
+	return c, nil
 }
---
+++ rpc/internal/client.go
@@ -0,0 +1,76 @@
+package internal
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+
+	"github.com/NethermindEth/starknet.go/rpc"
+)
+
+// @todo see docs
+
+// Do is a function that performs a remote procedure call (RPC) using the
+// provided callCloser.
+// It passes the parameters as an array in the JSON-RPC call.
+//
+// Parameters:
+//   - ctx: represents the current execution context
+//   - call: the callCloser object
+//   - method: the string representing the RPC method to be called
+//   - data: the interface{} to store the result of the RPC call
+//   - args: variadic and can be used to pass additional arguments to the RPC method
+//
+// Returns:
+//   - error: an error if any occurred during the function call
+func Do(
+	ctx context.Context,
+	c rpc.Caller,
+	method string,
+	data interface{},
+	args ...interface{},
+) error {
+	var raw json.RawMessage
+	err := c.CallContextWithSliceArgs(ctx, &raw, method, args...)
+	if err != nil {
+		return err
+	}
+	if len(raw) == 0 {
+		return errors.New("not found")
+	}
+	if err := json.Unmarshal(raw, &data); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// DoAsObject is a function that performs a remote procedure call (RPC) using
+// the provided callCloser. It passes the parameter as an object in the JSON-RPC
+// call, used for RPC-Calls with optional arguments since Juno doesn't support
+// optional arguments being passed in an array, only within an object.
+//
+// Parameters:
+//   - ctx: represents the current execution context
+//   - call: the callCloser object
+//   - method: the string representing the RPC method to be called
+//   - data: the interface{} to store the result of the RPC call
+//   - arg: the interface{} to pass as an object to the RPC method
+//
+// Returns:
+//   - error: an error if any occurred during the function call
+func DoAsObject(ctx context.Context, c rpc.Caller, method string, data, arg interface{}) error {
+	var raw json.RawMessage
+	err := c.CallContext(ctx, &raw, method, arg)
+	if err != nil {
+		return err
+	}
+	if len(raw) == 0 {
+		return errors.New("not found")
+	}
+	if err := json.Unmarshal(raw, &data); err != nil {
+		return err
+	}
+
+	return nil
+}
