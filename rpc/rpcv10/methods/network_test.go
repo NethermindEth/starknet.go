d0acf6248f8636982bf185c61291bb9d62ebf198
thiagodeev <thiagodeev@gmail.com>
2026-01-16 17:49:10 -0300

    refactor: join network methods in a single file

--- rpc/rpcv10/methods/network_test.go
+++ rpc/rpcv10/methods/network_test.go
@@ -11,6 +11,85 @@ import (
 	"go.uber.org/mock/gomock"
 )
 
+// TestChainID is a function that tests the ChainID function.
+func TestChainID(t *testing.T) {
+	tests.RunTestOn(
+		t,
+		tests.DevnetEnv,
+		tests.IntegrationEnv,
+		tests.MainnetEnv,
+		tests.MockEnv,
+		tests.TestnetEnv,
+	)
+
+	testConfig := internal.BeforeEach(t, false)
+
+	testCase := map[tests.TestEnv]string{
+		tests.DevnetEnv:      "SN_SEPOLIA",
+		tests.IntegrationEnv: "SN_INTEGRATION_SEPOLIA",
+		tests.MainnetEnv:     "SN_MAIN",
+		tests.MockEnv:        "SN_SEPOLIA",
+		tests.TestnetEnv:     "SN_SEPOLIA",
+	}[tests.TEST_ENV]
+
+	if tests.TEST_ENV == tests.MockEnv {
+		testConfig.MockClient.EXPECT().
+			CallContextWithSliceArgs(
+				t.Context(),
+				gomock.Any(),
+				"starknet_chainId",
+			).
+			DoAndReturn(func(_, result, _ any, _ ...any) error {
+				rawResp := result.(*json.RawMessage)
+				*rawResp = json.RawMessage("\"0x534e5f5345504f4c4941\"") // "SN_SEPOLIA"
+
+				return nil
+			}).
+			Times(1)
+	}
+
+	chain, err := ChainID(
+		t.Context(),
+		testConfig.Provider,
+	)
+	require.NoError(t, err)
+	require.Equal(t, testCase, chain)
+}
+
+// TestSpecVersion tests the SpecVersion function.
+func TestSpecVersion(t *testing.T) {
+	tests.RunTestOn(t, tests.MockEnv, tests.TestnetEnv, tests.IntegrationEnv)
+
+	testConfig := internal.BeforeEach(t, false)
+
+	if tests.TEST_ENV == tests.MockEnv {
+		testConfig.MockClient.EXPECT().
+			CallContextWithSliceArgs(
+				t.Context(),
+				gomock.Any(),
+				"starknet_specVersion",
+			).
+			DoAndReturn(func(_, result, _ any, _ ...any) error {
+				rawResp := result.(*json.RawMessage)
+				*rawResp = json.RawMessage("\"0.10.0\"")
+
+				return nil
+			}).
+			Times(1)
+	}
+
+	resp, err := SpecVersion(
+		t.Context(),
+		testConfig.Provider,
+	)
+	require.NoError(t, err)
+
+	rawExpectedResp := testConfig.RPCSpy.LastResponse()
+	rawResp, err := json.Marshal(resp)
+	require.NoError(t, err)
+	assert.Equal(t, string(rawExpectedResp), string(rawResp))
+}
+
 // TestSyncing tests the Syncing function.
 func TestSyncing(t *testing.T) {
 	tests.RunTestOn(t,
