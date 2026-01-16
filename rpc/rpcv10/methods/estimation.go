6aea7df4113fa0e413c008a880dfbff70e95d015
thiagodeev <thiagodeev@gmail.com>
2026-01-16 17:14:38 -0300

    refactor: join estimate methods in a single file

--- rpc/rpcv10/methods/estimation.go
+++ rpc/rpcv10/methods/estimation.go
@@ -47,3 +47,33 @@ func EstimateFee(
 
 	return raw, nil
 }
+
+// EstimateMessageFee estimates the L2 fee of a message sent on L1 (Provider struct).
+//
+// Parameters:
+//   - ctx: The context of the function call
+//   - msg: The message to estimate the fee for
+//   - blockID: The ID of the block to estimate the fee in
+//
+// Returns:
+//   - MessageFeeEstimation: the fee estimated for the message
+//   - error: an error if any occurred during the execution
+func EstimateMessageFee(
+	ctx context.Context,
+	c callers.Caller,
+	msg rpcv10.MsgFromL1,
+	blockID rpcv10.BlockID,
+) (rpcv10.MessageFeeEstimation, error) {
+	var raw rpcv10.MessageFeeEstimation
+	if err := internal.Do(
+		ctx, c, "starknet_estimateMessageFee", &raw, msg, blockID,
+	); err != nil {
+		return raw, rpcerr.UnwrapToRPCErr(err,
+			rpcv10.ErrContractError,
+			rpcv10.ErrContractNotFound,
+			rpcv10.ErrBlockNotFound,
+		)
+	}
+
+	return raw, nil
+}
