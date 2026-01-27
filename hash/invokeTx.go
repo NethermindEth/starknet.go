9ab136f6786f8fd5d3296135bd541497d52bbc22
thiagodeev <thiagodeev@gmail.com>
2026-01-27 10:50:06 -0300

    chore: removed complex invokehash functions and unused constraints

--- hash/invokeTx.go
+++ hash/invokeTx.go
@@ -3,66 +3,8 @@ package hash
 import (
 	"github.com/NethermindEth/juno/core/felt"
 	"github.com/NethermindEth/starknet.go/curve"
-	"github.com/NethermindEth/starknet.go/rpc/rpcv10"
-	"github.com/NethermindEth/starknet.go/rpc/rpcv9"
 	"github.com/NethermindEth/starknet.go/types/constraints"
 )
-
-// invokeV0 represents a pointer to an invoke V0
-// transaction from all supported RPC versions.
-type invokeV0 interface {
-	*rpcv9.InvokeTxnV0 | *rpcv10.InvokeTxnV0
-}
-
-// invokeV1 represents a pointer to an invoke V1
-// transaction from all supported RPC versions.
-type invokeV1 interface {
-	*rpcv9.InvokeTxnV1 | *rpcv10.InvokeTxnV1
-}
-
-// invokeV3 represents a pointer to an invoke V3
-// transaction from all supported RPC versions.
-type invokeV3 interface {
-	*rpcv9.InvokeTxnV3 | *rpcv10.InvokeTxnV3
-}
-
-// invokeTx represents a pointer to an invoke transaction
-// from all supported RPC versions.
-type invokeTx interface {
-	invokeV0 | invokeV1 | invokeV3
-}
-
-// @new
-// TransactionHashInvoke calculates the transaction hash for an invoke transaction.
-//
-// Parameters:
-//   - txn: The invoke transaction to calculate the hash for
-//   - chainID: The chain ID as a *felt.Felt
-//
-// Returns:
-//   - *felt.Felt: the calculated transaction hash
-//   - error: an error if any
-func TransactionHashInvoke[T invokeTx](tx T, chainID *felt.Felt) (*felt.Felt, error) {
-	switch typedTx := any(tx).(type) {
-	// **** v9 ****
-	case *rpcv9.InvokeTxnV0:
-		return TransactionHashInvokeV0(typedTx, chainID)
-	case *rpcv9.InvokeTxnV1:
-		return TransactionHashInvokeV1(typedTx, chainID)
-	case *rpcv9.InvokeTxnV3:
-		return TransactionHashInvokeV3(typedTx, chainID)
-	// **** v10 ****
-	case *rpcv10.InvokeTxnV0:
-		return TransactionHashInvokeV0(typedTx, chainID)
-	case *rpcv10.InvokeTxnV1:
-		return TransactionHashInvokeV1(typedTx, chainID)
-	case *rpcv10.InvokeTxnV3:
-		return TransactionHashInvokeV3(typedTx, chainID)
-	default:
-		// Should never happen due to the generic type constraint
-		return nil, errTxTypeNotSupported
-	}
-}
 
 // @changed this and all other functions are now generic
 // TransactionHashInvokeV0 calculates the transaction hash for a invoke V0 transaction.
